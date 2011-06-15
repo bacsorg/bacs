1c4330d913fdca47371b669d91739f6e6daae572
Aleksey Filippov <sarum9in@gmail.com>
2011-06-16 01:18:26 +0400

    C binding was added.

---
+++ bunsan_pm_c.cpp
@@ -0,0 +1,88 @@
+#include "bunsan_pm_c.hpp"
+
+#include <cstring>
+
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/info_parser.hpp>
+
+#include "repository.hpp"
+#include "util.hpp"
+
+void *bunsan_pm_create(char *error, size_t ebufsize, const char *config)
+{
+	try
+	{
+		boost::property_tree::ptree cfg;
+		bunsan::read_info(config, cfg);
+		bunsan::pm::repository *repo = new bunsan::pm::repository(cfg);
+		return repo;
+	}
+	catch (std::exception &e)
+	{
+		strncpy(error, e.what(), ebufsize);
+		return 0;
+	}
+	catch (...)
+	{
+		strncpy(error, "Unknown error", ebufsize);
+		return 0;
+	}
+}
+
+int bunsan_pm_clean(char *error, size_t ebufsize, void *repository)
+{
+	try
+	{
+		static_cast<bunsan::pm::repository *>(repository)->clean();
+		return 0;
+	}
+	catch (std::exception &e)
+	{
+		strncpy(error, e.what(), ebufsize);
+		return -2;
+	}
+	catch (...)
+	{
+		strncpy(error, "Unknown error", ebufsize);
+		return -1;
+	}
+}
+
+int bunsan_pm_extract(char *error, size_t ebufsize, void *repository, const char *package, const char *dir)
+{
+	try
+	{
+		static_cast<bunsan::pm::repository *>(repository)->extract(package, dir);
+		return 0;
+	}
+	catch (std::exception &e)
+	{
+		strncpy(error, e.what(), ebufsize);
+		return -2;
+	}
+	catch (...)
+	{
+		strncpy(error, "Unknown error", ebufsize);
+		return -1;
+	}
+}
+
+int bunsan_pm_remove(char *error, size_t ebufsize, void *repository)
+{
+	try
+	{
+		delete static_cast<bunsan::pm::repository *>(repository);
+		return 0;
+	}
+	catch (std::exception &e)
+	{
+		strncpy(error, e.what(), ebufsize);
+		return -2;
+	}
+	catch (...)
+	{
+		strncpy(error, "Unknown error", ebufsize);
+		return -1;
+	}
+}
+
