ed2611cab1650595698f19cfe072146b15898d59
Aleksey Filippov <sarum9in@gmail.com>
2011-08-06 14:08:45 +0400

    bunsan::pm::checksum cryptopp support: SHA512 hash function.

--- src/lib/checksum.cpp
+++ src/lib/checksum.cpp
@@ -1,24 +1,62 @@
 #include "bunsan/pm/checksum.hpp"
 
 #include <sstream>
+#include <iomanip>
 
 #include <boost/filesystem/fstream.hpp>
+
 #include <boost/crc.hpp>
+
+namespace
+{
+	std::string crc32_checksum(const boost::filesystem::path &file)
+	{
+		boost::crc_32_type crc;
+		boost::filesystem::ifstream in(file, std::ios_base::binary);// TODO error control
+		constexpr size_t bufsize = 1024;
+		char buf[bufsize];
+		do
+		{
+			in.read(buf, bufsize);
+			crc.process_bytes(buf, in.gcount());
+		}
+		while (in);
+		std::stringstream out;
+		out<<std::hex<<std::uppercase<<crc.checksum();
+		return out.str();
+	}
+}
+
+#include <cryptopp/sha.h>
+
+namespace
+{
+	template <typename HASH>
+	std::string CryptoPP_checksum(const boost::filesystem::path &file)
+	{
+		constexpr size_t bufsize = 1024;
+		byte buf[bufsize];
+		static_assert(sizeof(byte)==sizeof(char), "size of byte have to be equal size of char");
+		HASH hash;
+		boost::filesystem::ifstream in(file, std::ios_base::binary);// TODO error control
+		do
+		{
+			in.read(reinterpret_cast<char *>(buf), bufsize);
+			hash.Update(buf, in.gcount());
+		}
+		while (in);
+		byte out[HASH::DIGESTSIZE];
+		hash.Final(out);
+		std::stringstream sout;
+		static_assert(sizeof(byte)<=sizeof(int), "size of byte have to be not greater than size of int");
+		for (byte i: out)
+			sout<<std::hex<<std::uppercase<<std::setw(2)<<std::setfill('0')<<int(i);
+		return sout.str();
+	}
+}
 
 std::string bunsan::pm::checksum(const boost::filesystem::path &file)
 {
-	boost::crc_32_type crc;
-	boost::filesystem::ifstream in(file, std::ios_base::binary);// TODO error control
-	constexpr size_t bufsize = 1024;
-	char buf[bufsize];
-	do
-	{
-		in.read(buf, bufsize);
-		crc.process_bytes(buf, in.gcount());
-	}
-	while (in);
-	std::stringstream out;
-	out<<std::hex<<std::uppercase<<crc.checksum();
-	return out.str();
+	return CryptoPP_checksum<CryptoPP::SHA512>(file);
 }
 
