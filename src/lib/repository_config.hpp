aa4804888300056e6cd6fe6fb0b7b7cbef7ef90c
Aleksey Filippov <sarum9in@gmail.com>
2011-10-16 01:00:11 +0400

    src/lib/repository_config.hpp content was moved to include/bunsan/pm/index.hpp. src/lib/repository_config.hpp was declared deprecated.

--- src/lib/repository_config.hpp
+++ src/lib/repository_config.hpp
@@ -1,39 +1,11 @@
 #ifndef BUNSAN_PM_REPOSITORY_CONFIG_HPP
 #define BUNSAN_PM_REPOSITORY_CONFIG_HPP
 
-namespace
-{
-	constexpr const char *child_source = "source";
-	constexpr const char *child_import_source = "import.source";
-	constexpr const char *child_import_package = "import.package";
+#warning this header is deprecated, please use "bunsan/pm/index.hpp" instead
 
-	constexpr const char *dir_tmp = "dir.tmp";
-	constexpr const char *dir_package = "dir.package";
-	constexpr const char *dir_source = "dir.source";
+#include "bunsan/pm/index.hpp"
 
-	constexpr const char *name_file_tmp = "name.file.tmp";
-	constexpr const char *name_file_index = "name.file.index";
-	constexpr const char *name_file_checksum = "name.file.checksum";
-	constexpr const char *name_file_snapshot = "name.file.snapshot";
-	constexpr const char *name_file_pkg = "name.file.pkg";
-
-	constexpr const char *name_dir_pkg = "name.dir.pkg";
-	constexpr const char *name_dir_source = "name.dir.source";
-	constexpr const char *name_dir_build = "name.dir.build";
-
-	constexpr const char *suffix_src = "suffix.src";
-	constexpr const char *suffix_pkg = "suffix.pkg";
-
-	constexpr const char *command_fetch = "command.fetch";
-	constexpr const char *command_unpack = "command.unpack";
-	constexpr const char *command_configure = "command.configure";
-	constexpr const char *command_compile = "command.compile";
-	constexpr const char *command_pack = "command.pack";
-	constexpr const char *command_extract = "command.extract";
-	constexpr const char *command_create = "command.create";
-
-	constexpr const char *repository_url = "repository";
-}
+using namespace bunsan::pm::index;
 
 #endif //BUNSAN_PM_REPOSITORY_CONFIG_HPP
 
