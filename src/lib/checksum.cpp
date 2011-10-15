564ed7870429a94737a343a7916cc17abe108ed8
Aleksey Filippov <sarum9in@gmail.com>
2011-10-16 00:02:50 +0400

    bunsan::pm::checksum error control via exceptions.

--- src/lib/checksum.cpp
+++ src/lib/checksum.cpp
@@ -7,12 +7,16 @@
 
 #include <boost/crc.hpp>
 
+#include "bunsan/system_error.hpp"
+
 namespace
 {
 	std::string crc32_checksum(const boost::filesystem::path &file)
 	{
 		boost::crc_32_type crc;
-		boost::filesystem::ifstream in(file, std::ios_base::binary);// TODO error control
+		boost::filesystem::ifstream in(file, std::ios_base::binary);
+		if (!in.is_open())
+			bunsan::system_error("open file: \""+file.native()+"\"");
 		constexpr size_t bufsize = 1024;
 		char buf[bufsize];
 		do
@@ -21,6 +25,8 @@ namespace
 			crc.process_bytes(buf, in.gcount());
 		}
 		while (in);
+		if (in.bad())
+			bunsan::system_error("reading from file: \""+file.native()+"\"");
 		std::stringstream out;
 		out<<std::hex<<std::uppercase<<crc.checksum();
 		return out.str();
@@ -38,13 +44,17 @@ namespace
 		byte buf[bufsize];
 		static_assert(sizeof(byte)==sizeof(char), "size of byte have to be equal size of char");
 		HASH hash;
-		boost::filesystem::ifstream in(file, std::ios_base::binary);// TODO error control
+		boost::filesystem::ifstream in(file, std::ios_base::binary);
+		if (!in.is_open())
+			bunsan::system_error("open file: \""+file.native()+"\"");
 		do
 		{
 			in.read(reinterpret_cast<char *>(buf), bufsize);
 			hash.Update(buf, in.gcount());
 		}
 		while (in);
+		if (in.bad())
+			bunsan::system_error("reading from file: \""+file.native()+"\"");
 		byte out[HASH::DIGESTSIZE];
 		hash.Final(out);
 		std::stringstream sout;
