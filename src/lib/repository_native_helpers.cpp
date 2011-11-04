076e4873cf1871e0a900255c7a0592426c387a82
Aleksey Filippov <sarum9in@gmail.com>
2011-11-04 19:49:40 +0400

    bunsan::pm::repository::native::read_depends() implementation. repository_native_helpers reimplementation corresponding to library changes.

--- src/lib/repository_native_helpers.cpp
+++ src/lib/repository_native_helpers.cpp
@@ -1,5 +1,6 @@
 #include "repository_native.hpp"
-#include "repository_config.hpp"
+#include "bunsan/pm/index.hpp"
+#include "bunsan/pm/config.hpp"
 
 #include "bunsan/util.hpp"
 
@@ -10,30 +11,46 @@ std::string bunsan::pm::repository::native::value(const std::string &key)
 
 void bunsan::pm::repository::native::read_index(const entry &package, boost::property_tree::ptree &ptree)
 {
-	boost::property_tree::read_info(source_resource(package, value(name_file_index)).generic_string(), ptree);
+	boost::property_tree::read_info(source_resource(package, value(config::name::file::index)).generic_string(), ptree);
 }
 
 void bunsan::pm::repository::native::read_checksum(const entry &package, boost::property_tree::ptree &ptree)
 {
-	boost::property_tree::read_info(source_resource(package, value(name_file_checksum)).generic_string(), ptree);
+	boost::property_tree::read_info(source_resource(package, value(config::name::file::checksum)).generic_string(), ptree);
 }
 
 std::string bunsan::pm::repository::native::remote_resource(const entry &package, const std::string &name)
 {
-	return package.remote_resource(value(repository_url), name);
+	return package.remote_resource(value(config::repository_url), name);
 }
 
 boost::filesystem::path bunsan::pm::repository::native::source_resource(const entry &package, const std::string &name)
 {
-	return package.local_resource(value(dir_source), name);
+	return package.local_resource(value(config::dir::source), name);
 }
 
 boost::filesystem::path bunsan::pm::repository::native::package_resource(const entry &package, const std::string &name)
 {
-	return package.local_resource(value(dir_package), name);
+	return package.local_resource(value(config::dir::package), name);
 }
 
 bunsan::pm::repository::native::native(const boost::property_tree::ptree &config_): config(config_){}
+
+bunsan::pm::depends bunsan::pm::repository::native::read_depends(const entry &package)
+{
+	boost::property_tree::ptree index;
+	read_index(package, index);
+	depends deps;
+	for (const auto &i: index.get_child(pm::index::package))
+		deps.package.insert(std::make_pair(i.first, i.second.get_value<std::string>()));
+	for (const auto &i: index.get_child(pm::index::source::self))
+		deps.source.self.insert(std::make_pair(i.first, i.second.get_value<std::string>()));
+	for (const auto &i: index.get_child(pm::index::source::import::package))
+		deps.source.import.package.insert(std::make_pair(i.first, i.second.get_value<std::string>()));
+	for (const auto &i: index.get_child(pm::index::source::import::source))
+		deps.source.import.source.insert(std::make_pair(i.first, i.second.get_value<std::string>()));
+	return deps;
+}
 
 namespace
 {
@@ -63,18 +80,18 @@ namespace
 void bunsan::pm::repository::native::check_dirs()
 {
 	DLOG(checking directories);
-	check_dir(value(dir_source));
-	check_dir(value(dir_package));
-	check_dir(value(dir_tmp));
+	check_dir(value(config::dir::source));
+	check_dir(value(config::dir::package));
+	check_dir(value(config::dir::tmp));
 	DLOG(checked);
 }
 
 void bunsan::pm::repository::native::clean()
 {
 	DLOG(trying to clean cache);
-	bunsan::reset_dir(value(dir_source));
-	bunsan::reset_dir(value(dir_package));
-	bunsan::reset_dir(value(dir_tmp));
+	bunsan::reset_dir(value(config::dir::source));
+	bunsan::reset_dir(value(config::dir::package));
+	bunsan::reset_dir(value(config::dir::tmp));
 	DLOG(cleaned);
 }
 
