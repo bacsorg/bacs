ec28ff742ebeeaf0bf4963779c230ee5a27dfa73
Aleksey Filippov <sarum9in@gmail.com>
2011-11-05 22:58:20 +0400

    Configuration update + documentation (partly).

--- include/bunsan/pm/config.hpp
+++ include/bunsan/pm/config.hpp
@@ -5,43 +5,44 @@ namespace bunsan{namespace pm{namespace config
 {
 	namespace dir
 	{
-		constexpr const char *tmp = "dir.tmp";
-		constexpr const char *package = "dir.package";
-		constexpr const char *source = "dir.source";
+		constexpr const char *tmp = "dir.tmp";		///< directory for large temporary files
+		constexpr const char *package = "dir.package";	///< directory for built packages
+		constexpr const char *source = "dir.source";	///< directory for package sources
 	}
+	/// file and directory names, usually relative
 	namespace name
 	{
 		namespace file
 		{
-			constexpr const char *tmp = "name.file.tmp";
-			constexpr const char *index = "name.file.index";
-			constexpr const char *checksum = "name.file.checksum";
-			constexpr const char *snapshot = "name.file.snapshot";
-			constexpr const char *pkg = "name.file.pkg";
+			constexpr const char *tmp = "name.file.tmp";					///< file mask for small temporary files, should be absolute
+			constexpr const char *index = "name.file.index";				///< index file name
+			constexpr const char *checksum = "name.file.checksum";				///< checksum file name
+			constexpr const char *build_snapshot = "name.file.build_snapshot";		///< build snapshot file name
+			constexpr const char *installation_snapshot = "name.file.installation_snapshot";///< installation snapshot file name
+			constexpr const char *build = "name.file.build";				///< build archive name
+			constexpr const char *installation = "name.file.installation";			///< installation archive name
 		}
 		namespace dir
 		{
-			constexpr const char *pkg = "name.dir.pkg";
-			constexpr const char *source = "name.dir.source";
-			constexpr const char *build = "name.dir.build";
+			constexpr const char *source = "name.dir.source";		///< subdirectory for package source
+			constexpr const char *build = "name.dir.build";			///< subdirectory for package building
+			constexpr const char *installation = "name.dir.installation";	///< subdirectory for package installation
 		}
 	}
 	namespace suffix
 	{
-		constexpr const char *src = "suffix.src";
-		constexpr const char *pkg = "suffix.pkg";
+		constexpr const char *archive = "suffix.archive";	///< suffix of archive files
 	}
 	namespace command
 	{
-		constexpr const char *fetch = "command.fetch";
-		constexpr const char *unpack = "command.unpack";
-		constexpr const char *configure = "command.configure";
-		constexpr const char *compile = "command.compile";
-		constexpr const char *pack = "command.pack";
-		constexpr const char *extract = "command.extract";
-		constexpr const char *create = "command.create";
+		constexpr const char *fetch = "command.fetch";		///< download command: fetch(source, destination)
+		constexpr const char *unpack = "command.unpack";	///< unpack command: unpack(source, destination)
+		constexpr const char *pack = "command.pack";		///< pack command, \todo encapsulation fault documentation
+		constexpr const char *configure = "command.configure";	///< configure command
+		constexpr const char *compile = "command.compile";	///< compilation command
+		constexpr const char *install = "command.install";	///< install command
 	}
-	constexpr const char *repository_url = "repository";
+	constexpr const char *repository_url = "repository_url";	///< repository location
 }}}
 
 #endif //BUNSAN_PM_CONFIG_HPP
