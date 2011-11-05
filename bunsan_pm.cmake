2c08b971bc1f343f902073f865bd300f28431dac
Aleksey Filippov <sarum9in@gmail.com>
2011-11-05 22:27:07 +0400

    Obsolete files deletion.

--- bunsan_pm.cmake
+++
@@ -1,9 +0,0 @@
-function(bunsan_pm_cpack)
-	set(CPACK_GENERATOR "TGZ")
-	set(CPACK_PACKAGE_VERSION_MAJOR 0)
-	set(CPACK_PACKAGE_VERSION_MINOR 0)
-	set(CPACK_PACKAGE_VERSION_PATCH 0)
-	set(CPACK_PACKAGE_FILE_NAME "pkg")
-	include(CPack)
-endfunction()
-
