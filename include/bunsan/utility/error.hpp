2479e23c2b7319fa931d16fb3694df03ca1f93da
Aleksey Filippov <sarum9in@gmail.com>
2012-01-25 03:07:26 +0400

    bunsan::utility::error implementation.

---
+++ include/bunsan/utility/error.hpp
@@ -0,0 +1,15 @@
+#ifndef BUNSAN_UTILITY_ERROR_HPP
+#define BUNSAN_UTILITY_ERROR_HPP
+
+#include "bunsan/error.hpp"
+
+namespace bunsan{namespace utility
+{
+	struct error: virtual bunsan::error
+	{
+		explicit error(const std::string &msg);
+	};
+}}
+
+#endif //BUNSAN_UTILITY_ERROR_HPP
+
---
+++ src/error.cpp
@@ -0,0 +1,4 @@
+#include "bunsan/utility/error.hpp"
+
+bunsan::utility::error::error(const std::string &msg): bunsan::error(msg) {}
+
