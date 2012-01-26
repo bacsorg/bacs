634d2af7021aad80991b110daa88baaca0f22d06
Aleksey Filippov <sarum9in@gmail.com>
2012-01-26 17:26:44 +0400

    bunsan::utility::resolver stub.

--- src/resolver.cpp
+++ src/resolver.cpp
@@ -1,2 +1,19 @@
 #include "bunsan/utility/resolver.hpp"
 
+#include <boost/process.hpp>
+
+bunsan::utility::resolver::resolver() {}
+
+bunsan::utility::resolver::resolver(const boost::property_tree::ptree &) {}
+
+boost::filesystem::path bunsan::utility::resolver::find_executable(const boost::filesystem::path &exe)
+{
+	return boost::process::find_executable_in_path(exe.filename().string());
+}
+
+boost::filesystem::path bunsan::utility::resolver::find_library(const boost::filesystem::path &lib)
+{
+#warning unimplemented
+	return boost::filesystem::path();
+}
+
