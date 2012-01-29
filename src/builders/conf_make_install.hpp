02a8e954cd7da19286fc8436ee44cb591519eea0
Aleksey Filippov <sarum9in@gmail.com>
2012-01-30 03:45:35 +0400

    conf_make_install class implementation (derived from bunsan::utility::builder).

---
+++ src/builders/conf_make_install.hpp
@@ -0,0 +1,29 @@
+#ifndef SRC_BUILDERS_CONF_MAKE_INSTALL_HPP
+#define SRC_BUILDERS_CONF_MAKE_INSTALL_HPP
+
+#include "bunsan/utility/builder.hpp"
+
+namespace bunsan{namespace utility{namespace builders
+{
+	class conf_make_install: public builder
+	{
+		virtual void install(
+			const boost::filesystem::path &src,
+			const boost::filesystem::path &bin,
+			const boost::filesystem::path &root);
+	protected:
+		virtual void configure_(
+			const boost::filesystem::path &src,
+			const boost::filesystem::path &bin)=0;
+		virtual void make_(
+			const boost::filesystem::path &src,
+			const boost::filesystem::path &bin)=0;
+		virtual void install_(
+			const boost::filesystem::path &src,
+			const boost::filesystem::path &bin,
+			const boost::filesystem::path &root)=0;
+	};
+}}}
+
+#endif //SRC_BUILDERS_CONF_MAKE_INSTALL_HPP
+
