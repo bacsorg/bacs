3e99e2af549f08ee084782ee65e3ed5fe437178b
Aleksey Filippov <sarum9in@gmail.com>
2012-02-03 22:53:21 +0400

    bunsan::utility::builder::cmake generators implementation using bunsan::utility::maker.

--- src/builders/cmake.cpp
+++ src/builders/cmake.cpp
@@ -1,5 +1,7 @@
 #include "cmake.hpp"
 
+#include <algorithm>
+
 #include "bunsan/process/execute.hpp"
 
 using namespace bunsan::utility;
@@ -11,16 +13,85 @@ bool builders::cmake::factory_reg_hook = builder::register_new("cmake",
 		return ptr;
 	});
 
-builders::cmake::cmake(const resolver &resolver_):
-	m_resolver(resolver_), m_cmake_exe(m_resolver.find_executable("cmake")) {}
+void builders::cmake::setup_generator()
+{
+#if defined(BOOST_POSIX_API)
+	set_generator("Unix Makefiles");
+#elif defined(BOOST_WINDOWS_API)
+	set_generator("NMake Makefiles");
+#else
+#warning unknown platform, unknown default generator
+	BOOST_THROW_EXCEPTION(error("Unknown platform, you have to specify generator"));
+#endif
+}
 
-std::vector<std::string> builders::cmake::argv_(const boost::filesystem::path &src)
+enum class builders::cmake::generator::type: std::size_t
+{
+	makefile,
+	nmakefile,
+	visual_studio,
+	unknown
+};
+
+const std::vector<builders::cmake::generator> builders::cmake::generators =
+	{
+		// makefiles
+		{"Unix Makefiles", generator::type::makefile},
+		{"MinGW Makefiles", generator::type::makefile},
+		{"MSYS Makefiles", generator::type::makefile},
+		{"NMake Makefiles", generator::type::makefile},
+		{"NMake Makefiles JOM", generator::type::makefile},
+		{"Borland Makefiles", generator::type::makefile},
+		{"Watcom WMake", generator::type::unknown},
+		// IDEs
+		// CodeBlocks
+		{"CodeBlocks - Unix Makefiles", generator::type::makefile},
+		{"CodeBlocks - MinGW Makefiles", generator::type::makefile},
+		{"CodeBlocks - NMake Makefiles", generator::type::nmakefile},
+		// Eclipse
+		{"Eclipse CDT4 - Unix Makefiles", generator::type::makefile},
+		{"Eclipse CDT4 - MinGW Makefiles", generator::type::makefile},
+		{"Eclipse CDT4 - NMake Makefiles", generator::type::nmakefile},
+		// KDevelop
+		{"KDevelop3", generator::type::unknown},
+		{"KDevelop3 - Unix Makefiles", generator::type::makefile},
+		// Visual Studio
+		{"Visual Studio 10", generator::type::visual_studio},
+		{"Visual Studio 10 IA64", generator::type::visual_studio},
+		{"Visual Studio 10 Win64", generator::type::visual_studio},
+		{"Visual Studio 11", generator::type::visual_studio},
+		{"Visual Studio 11 Win64", generator::type::visual_studio},
+		{"Visual Studio 6", generator::type::visual_studio},
+		{"Visual Studio 7", generator::type::visual_studio},
+		{"Visual Studio 7 .NET 2003", generator::type::visual_studio},
+		{"Visual Studio 8 2005", generator::type::visual_studio},
+		{"Visual Studio 8 2005 Win64", generator::type::visual_studio},
+		{"Visual Studio 9 2008", generator::type::visual_studio},
+		{"Visual Studio 9 2008 IA64", generator::type::visual_studio},
+		{"Visual Studio 9 2008 Win64", generator::type::visual_studio}
+	};
+
+builders::cmake::cmake(const resolver &resolver_):
+	m_resolver(resolver_), m_cmake_exe(m_resolver.find_executable("cmake"))
+{
+	setup_generator();
+}
+
+std::vector<std::string> builders::cmake::argv_(const boost::filesystem::path &src) const
 {
 	std::vector<std::string> argv;
 	argv.push_back(m_cmake_exe.filename().string());
+	if (m_generator)
+	{
+		argv.push_back("-G");
+		argv.push_back(get_generator().m_id);
+	}
+	for (const auto &i: m_cmake_defines)
+	{
+		// TODO arguments check
+		argv.push_back("-D"+i.first+"="+i.second);
+	}
 	argv.push_back(boost::filesystem::absolute(src).string());
-#warning configuration is not supported
-	//TODO config
 	return argv;
 }
 
@@ -39,16 +110,20 @@ void builders::cmake::make_(
 	const boost::filesystem::path &src,
 	const boost::filesystem::path &bin)
 {
-	bunsan::process::context ctx;
-#warning only make implementation
-	if (true)
+	BOOST_ASSERT(m_generator);
+	switch (generators[m_generator.get()].m_type)
 	{
-		const boost::filesystem::path m_make_exe =
-			m_resolver.find_executable("make");
-		ctx.executable(m_make_exe);
-		ctx.current_path(bin);
+	case generator::type::makefile:
+		{
+			maker_ptr ptr = maker::instance("make", m_resolver);
+			ptr->exec(bin, {});
+		}
+		break;
+	case generator::type::nmakefile:
+	case generator::type::visual_studio:
+	default:
+		BOOST_THROW_EXCEPTION(error("unknown cmake generator"));
 	}
-	bunsan::process::check_sync_execute(ctx);
 }
 
 void builders::cmake::install_(
@@ -56,24 +131,85 @@ void builders::cmake::install_(
 	const boost::filesystem::path &bin,
 	const boost::filesystem::path &root)
 {
-	bunsan::process::context ctx;
-#warning only make implementation
-	if (true)
+	BOOST_ASSERT(m_generator);
+	switch (generators[m_generator.get()].m_type)
 	{
-		const boost::filesystem::path m_make_exe =
-			m_resolver.find_executable("make");
-		ctx.executable(m_make_exe);
-		ctx.current_path(bin);
-		ctx.argv({
-				m_make_exe.filename().string(),
-				"install",
-				"DESTDIR="+boost::filesystem::absolute(root).string()
-			});
+	case generator::type::makefile:
+		{
+			maker_ptr ptr = maker::instance("make", m_resolver);
+			utility::config_type config;
+			config.put("defines.DESTDIR", boost::filesystem::absolute(root).string());
+			ptr->setup(config);
+			ptr->exec(bin, {"install"});
+		}
+		break;
+	case generator::type::nmakefile:
+	case generator::type::visual_studio:
+	default:
+		BOOST_THROW_EXCEPTION(error("unknown cmake generator"));
 	}
-	bunsan::process::check_sync_execute(ctx);
 }
 
+void builders::cmake::set_generator(const std::string &gen_id)
+{
+	const std::size_t gen =
+		std::find_if(generators.begin(), generators.end(),
+			[&gen_id](const generator &gen)
+			{
+				return gen.m_id==gen_id;
+			})-generators.begin();
+	if (gen>=generators.size())
+		BOOST_THROW_EXCEPTION(error("Unknown generator id: \""+gen_id+"\""));
+	m_generator = gen;
+}
+
+const builders::cmake::generator &builders::cmake::get_generator() const
+{
+	BOOST_ASSERT(m_generator);
+	return generators[m_generator.get()];
+}
+
+/*!
+\verbatim
+cmake
+{
+	defines
+	{
+		CMAKE_INSTALL_PREFIX "/usr"
+	}
+	generator "Unix Makefiles"
+}
+FIXME
+;make
+{
+	; see make documentation
+}
+\endverbatim
+*/
 void builders::cmake::setup(const utility::config_type &config)
 {
+	utility::config_type make_config;
+	m_generator.reset();
+	m_cmake_defines.clear();
+	for (const auto &i: config)
+	{
+		if (i.first=="cmake")
+			for (const auto &j: i.second)
+			{
+				if (j.first=="defines")
+					for (const auto d: j.second)
+						m_cmake_defines[d.first] = d.second.get_value<std::string>();
+				else if (j.first=="generator")
+					set_generator(j.second.get_value<std::string>());
+				else
+					BOOST_THROW_EXCEPTION(unknown_option_error(i.first+"."+j.first));
+			}
+		else if (i.first=="make")
+			// TODO merge?
+			make_config = i.second;
+		else
+			BOOST_THROW_EXCEPTION(unknown_option_error(i.first));
+	}
+	setup_generator();
 }
 
--- src/builders/cmake.hpp
+++ src/builders/cmake.hpp
@@ -1,6 +1,14 @@
 #ifndef SRC_BUILDERS_CMAKE_HPP
 #define SRC_BUILDERS_CMAKE_HPP
 
+#include <map>
+#include <string>
+#include <vector>
+
+#include <boost/optional.hpp>
+
+#include "bunsan/utility/maker.hpp"
+
 #include "conf_make_install.hpp"
 
 namespace bunsan{namespace utility{namespace builders
@@ -13,8 +21,6 @@ namespace bunsan{namespace utility{namespace builders
 		explicit cmake(const resolver &resolver_);
 		virtual void setup(const utility::config_type &config);
 	protected:
-		std::vector<std::string> argv_(
-			const boost::filesystem::path &src);
 		virtual void configure_(
 			const boost::filesystem::path &src,
 			const boost::filesystem::path &bin);
@@ -26,6 +32,22 @@ namespace bunsan{namespace utility{namespace builders
 			const boost::filesystem::path &bin,
 			const boost::filesystem::path &root);
 	private:
+		struct generator
+		{
+			enum class type: std::size_t;
+			const std::string m_id;
+			const type m_type;
+		};
+	private:
+		void setup_generator();
+		void set_generator(const std::string &gen_id);
+		const generator &get_generator() const;
+		std::vector<std::string> argv_(
+			const boost::filesystem::path &src) const;
+	private:
+		boost::optional<std::size_t> m_generator;
+		std::map<std::string, std::string> m_cmake_defines;
+		static const std::vector<generator> generators;
 		static bool factory_reg_hook;
 	};
 }}}
