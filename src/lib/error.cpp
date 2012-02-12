cd5ae3ca67c8e8f43ed1b0f1d53ffd03b40c5927
Aleksey Filippov <sarum9in@gmail.com>
2012-02-12 23:13:26 +0400

    bunsan::pm::invalid_configuration implementation.

--- src/lib/error.cpp
+++ src/lib/error.cpp
@@ -1,4 +1,8 @@
 #include "bunsan/pm/error.hpp"
 
-bunsan::pm::error::error(const std::string &message_): bunsan::error(message_) {}
+bunsan::pm::error::error(const std::string &message_):
+	bunsan::error(message_) {}
+
+bunsan::pm::invalid_configuration::invalid_configuration(const std::string &message_):
+	bunsan::pm::error(message_) {}
 
