1af3ae449515c35296880eaacbcb05d42e31ba59
Aleksey Filippov <sarum9in@gmail.com>
2012-05-15 18:50:19 +0400

    repository partial documentation.

--- include/bacs/archive/repository.hpp
+++ include/bacs/archive/repository.hpp
@@ -13,24 +13,45 @@ namespace bacs{namespace archive
 {
     namespace problem
     {
+        /// problem id
         typedef std::string id;
+        /// information about problem, see problem.xsd from BACS.XSD
         typedef std::vector<unsigned char> info;
+        /// package name
         typedef bunsan::pm::entry package;
+        /// hash string
+        typedef std::vector<unsigned char> hash;
         typedef std::vector<id> id_list;
         typedef std::vector<info> info_list;
         typedef std::vector<package> package_list;
+        typedef std::vector<hash> hash_list;
     }
     class repository
     {
     public:
         explicit repository(const boost::property_tree::ptree &config_);
         // container
+        /*!
+         * \brief insert problems into repository
+         *
+         * \return list of inserted problem ids
+         */
         problem::id_list insert(/*archive*/);
+        /*!
+         * \brief erase problems from repository
+         *
+         * \return list of erased problems
+         */
         problem::id_list erase(const problem::id_list &id_list);
+        /*!
+         * \brief list of available problems
+         */
         problem::id_list all();
         // info
         problem::info info(const problem::id &id);
         problem::info_list info(const problem::id_list &id_list);
+        problem::hash hash(const problem::id &id);
+        problem::hash_list hash(const problem::id_list &id_list);
     private:
     };
 }}
