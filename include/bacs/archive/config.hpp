c37b4110a1b6f01aedf666fbd82f5a3f76f2d3ff
Aleksey Filippov <sarum9in@gmail.com>
2012-06-10 03:02:07 +0400

    bacs::archive::config partial definition.

---
+++ include/bacs/archive/config.hpp
@@ -0,0 +1,11 @@
+#ifndef BACS_ARCHIVE_CONFIG_HPP
+#define BACS_ARCHIVE_CONFIG_HPP
+
+namespace bacs{namespace archive{namespace config
+{
+    constexpr const char *resolver = "resolver";
+    constexpr const char *tmpdir = "tmpdir";
+}}}
+
+#endif //BACS_ARCHIVE_CONFIG_HPP
+
