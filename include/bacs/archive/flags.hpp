90e1184691b8712c44c82fc52b8f83cce0af0b8c
Aleksey Filippov <sarum9in@gmail.com>
2012-06-17 03:17:53 +0400

    bacs::archive::problem::flag::ignore declaration.
    New namespace: bacs::archive::problem::flag.
    New header: bacs/archive/flags.hpp.

---
+++ include/bacs/archive/flags.hpp
@@ -0,0 +1,10 @@
+#ifndef INCLUDE_BACS_ARCHIVE_FLAGS_HPP
+#define INCLUDE_BACS_ARCHIVE_FLAGS_HPP
+
+namespace bacs{namespace archive{namespace problem{namespace flag
+{
+    constexpr const char *ignore = "ignore";
+}}}}
+
+#endif //INCLUDE_BACS_ARCHIVE_FLAGS_HPP
+
