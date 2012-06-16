a01cb30c9f85c1cb9fdc70b91467c9f03316d10d
Aleksey Filippov <sarum9in@gmail.com>
2012-06-16 22:50:38 +0400

    repository interface major changes.
    Changes:
        *) erase functions were removed
        *) flag system
        *) id_list -> id_set
        *) new functions
            *) status()
            *) available()
            *) ignore()

--- include/bacs/archive/problem.hpp
+++ include/bacs/archive/problem.hpp
@@ -15,18 +15,28 @@ namespace bacs{namespace archive{namespace problem
     typedef std::string id;
     typedef std::vector<unsigned char> binary;
     /// information about problem, see problem.xsd from BACS.XSD
-    typedef binary info;
+    typedef binary info_type;
     /// package name
     typedef bunsan::pm::entry package;
     /// hash string
-    typedef binary hash;
-    typedef std::vector<id> id_list;
-    typedef std::map<id, info> info_map;
-    typedef std::map<id, hash> hash_map;
+    typedef binary hash_type;
+    typedef std::vector<id> id_set;
+    typedef std::map<id, info_type> info_map;
+    typedef std::map<id, hash_type> hash_map;
+    typedef std::string flag_type;
+    typedef std::set<flag_type> flag_set;
+    struct status
+    {
+        bool exists;
+        flag_set flags;
+        hash_type hash;
+    };
+    typedef std::map<id, status> status_map;
     struct import_info
     {
         bool ok;
         std::string error;
+        status previous, current;
     };
     struct archiver_config
     {
--- include/bacs/archive/repository.hpp
+++ include/bacs/archive/repository.hpp
@@ -22,89 +22,198 @@ namespace bacs{namespace archive
         explicit repository(const boost::property_tree::ptree &config_);
         /* container */
         /*!
-         * \brief insert problems into repository
+         * \brief Insert problems from archive into repository.
          *
          * \return import information
+         *
+         * Not atomic.
+         *
+         * \see repository::insert
          */
         problem::import_map insert_all(const problem::archiver_config &archiver_config, const boost::filesystem::path &archive);
         /*!
-         * \brief extract problems from repository
-         *
-         * \return archive with problems
-         *
-         * If problem does not exists for given id, this id is ignored.
-         */
-        bunsan::tempfile extract_all(const problem::archiver_config &archiver_config, const problem::id_list &id_list);
-        /*!
-         * \brief extract problem from repository
-         *
-         * \return false if problem does not exist
-         */
-        bool extract(const problem::id &id, const boost::filesystem::path &dst);
-        /*!
-         * \brief insert particular problem into repository
+         * \brief Insert particular problem into repository.
          *
          * \param location -- directory with problem
+         *
+         * If conversion is successful, problem flags are reset and data is updated.
+         * Otherwise does nothing.
+         *
+         * Atomic; exclusive-lock on success, lock-free otherwise.
+         *
+         * \see repository::reset_flags
          */
         problem::import_info insert(const problem::id &id, const boost::filesystem::path &location);
         /*!
-         * \brief erase problems from repository
+         * \brief Extract problems from repository.
          *
-         * If problem does not exists id is ignored.
+         * \return archive with problems
          *
-         * \return list of erased problems
+         * Ignores unavailable problems.
+         *
+         * Not atomic.
+         *
+         * \see repository::available
+         * \see repository::extract
          */
-        problem::id_list erase_all(const problem::id_list &id_list);
+        bunsan::tempfile extract_all(const problem::id_set &id_set, const problem::archiver_config &archiver_config);
         /*!
-         * \brief erase problem from repository
+         * \brief Extract problem from repository.
          *
-         * \todo Should problem be moved into backup storage?
-         * Or it simply mark it 'erased'? \see repository::repack
+         * \return false if problem is not available
          *
-         * \return false if problem does not exists
+         * Atomic, shared-lock.
+         *
+         * \see repository::available
          */
-        bool erase(const problem::id &id);
+        bool extract(const problem::id &id, const boost::filesystem::path &dst);
         /*!
-         * \brief list of available problems
+         * \brief Change problem id and repack.
+         *
+         * Atomic, exclusive-lock.
+         *
+         * \see repository::repack
          */
-        problem::id_list all();
-        /* info */
+        problem::import_info rename(const problem::id &current, const problem::id &future);
         /*!
-         * \brief get problem info
+         * \brief Set of available problems.
          *
-         * \return uninitialized optional if problem does not exist
+         * \see repository::available
          */
-        boost::optional<problem::info> info(const problem::id &id);
+        problem::id_set all();
         /*!
-         * \brief get info map for given problems
+         * \brief Check problem for existence.
          *
-         * If problem does not exist id is ignored.
-         */
-        problem::info_map info_all(const problem::id_list &id_list);
-        /*!
-         * \brief get problem hash
+         * Albeit problem exists, it may be ignored
+         * by some functions.
          *
-         * \return unitialized optional if problem does not exist
-         */
-        boost::optional<problem::hash> hash(const problem::id &id);
-        /*!
-         * \brief get hash map for given problems
+         * Atomic, lock-free.
          *
-         * If problem does not exist id is ignored.
-         */
-        problem::hash_map hash_all(const problem::id_list &id_list);
-        /*!
-         * \brief check problem existence
-         *
-         * Operation is atomic, lock-free
-         *
-         * \todo consistency: if 'erased' is a mark
-         * then repository::exists should have at least two variants (for mark and for real existence)
-         * \see repository::valid
+         * \see repository::available
          */
         bool exists(const problem::id &id);
         /*!
-         * \brief repack problem
+         * \brief Check problem for availability.
+         *
+         * Return true if problem is available and
+         * is not marked by 'ignore' flag.
+         *
+         * Atomic, shared-lock.
+         *
+         * \see repository::exists
+         * \see repository::has_flag
+         */
+        bool available(const problem::id &id);
+        /* flags */
+        /*!
+         * \brief Get problem status.
+         *
+         * Atomic, shared-lock.
+         */
+        problem::status status(const problem::id &id);
+        /*!
+         * \brief Get problems status.
+         *
+         * Not atomic.
+         *
+         * \see repository::status
+         */
+        problem::status_map status_all(const problem::id_set &id_set);
+        /*!
+         * \brief Check problem for flag.
+         *
+         * Atomic, lock-free.
+         */
+        bool has_flag(const problem::id &id, const problem::flag &flag);
+        /*!
+         * \brief Set problem flag.
+         *
+         * Atomic, exclusive-lock.
+         */
+        bool set_flag(const problem::id &id, const problem::flag &flag);
+        /*!
+         * \brief Set problem flags.
+         *
+         * Atomic, exclusive-lock.
+         */
+        bool set_flags(const problem::id &id, const problem::flag_set &flags);
+        /*!
+         * \brief Unset problem flag.
+         *
+         * Atomic, exclusive-lock.
+         */
+        bool unset_flag(const problem::id &id, const problem::flag &flag);
+        /*!
+         * \brief Unset problem flags.
+         *
+         * Atomic, exclusive-lock.
+         */
+        bool unset_flags(const problem::id &id, const problem::flag_set &flags);
+        /*!
+         * \brief Reset problem flags.
+         *
+         * Atomic, exclusive-lock.
+         */
+        bool reset_flags(const problem::id &id);
+        /*!
+         * \brief Mark problems with 'ignore' flag.
+         *
+         * If problem does not exists id is ignored.
+         *
+         * \return set of marked problems
+         *
+         * Not atomic.
+         *
+         * \see repository::ignore
+         */
+        problem::id_set ignore_all(const problem::id_set &id_set);
+        /*!
+         * \brief Alias for set_flag(id, "ignore").
+         *
+         * \return false if problem does not exists
+         *
+         * \see repository::set_flag
+         *
+         * \todo pair function, function rename possible (as flag)
+         */
+        bool ignore(const problem::id &id);
+        /* info */
+        /*!
+         * \brief Get problem info.
+         *
+         * \return uninitialized optional if problem is not available
+         *
+         * \see repository::available
+         */
+        boost::optional<problem::info> info(const problem::id &id);
+        /*!
+         * \brief Get info map for given problems.
+         *
+         * If problem is not available, id is ignored.
+         *
+         * \see repository::available
+         * \see repository::info
+         */
+        problem::info_map info_all(const problem::id_set &id_set);
+        /*!
+         * \brief Get problem hash.
+         *
+         * \return uninitialized optional if problem is not available
+         *
+         * \see repository::available
+         */
+        boost::optional<problem::hash_type> hash(const problem::id &id);
+        /*!
+         * \brief Get hash map for given problems.
+         *
+         * If problem is not available, id is ignored.
+         *
+         * \see repository::available
+         * \see repository::hash
+         */
+        problem::hash_map hash_all(const problem::id_set &id_set);
+        /*!
+         * \brief Repack problem.
          *
          * Repacking is similar to import
          * except using internal copy of problem
@@ -112,21 +221,33 @@ namespace bacs{namespace archive
          * Can be useful if problem conversion utility is changed
          * or index is corrupted
          *
-         * \todo inconsistent API: if repack is failed, problem is still present.
-         * It may be useful to mark problem 'erased', depending on repository::erase
-         * implementation.
+         * If repack is not successful problem is marked by 'ignore' flag.
+         *
+         * Atomic, exclusive-lock.
+         *
+         * \see repository::set_flag
          */
         problem::import_info repack(const problem::id &id);
         /*!
-         * \brief repack all given problems
+         * \brief Repack all given problems.
+         *
+         * Not atomic.
+         *
+         * \see repository::repack
          */
-        problem::import_map repack_all(const problem::id_list &id_list);
+        problem::import_map repack_all(const problem::id_set &id_set);
     private:
         /*!
-         * \brief Check problem for validity
+         * \brief Check problem for validity.
+         *
+         * \return true if problem exists and problem index has valid format
          *
          * Check basic format conformance, it does not check
          * whether problem is importable
+         *
+         * Atomic, shared-lock.
+         *
+         * \see repository::exists
          */
         bool valid(const problem::id &id);
 
