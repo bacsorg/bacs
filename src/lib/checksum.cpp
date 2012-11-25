38e2da1d087285bcffb5da5c8bb3c42ae96071d8
Aleksey Filippov <sarum9in@gmail.com>
2012-11-25 19:58:51 +0400

    checksum() refactoring.

--- src/lib/checksum.cpp
+++ src/lib/checksum.cpp
@@ -1,13 +1,12 @@
 #include "bunsan/pm/checksum.hpp"
-
-#include "bunsan/system_error.hpp"
+#include "bunsan/filesystem/fstream.hpp"
+#include "bunsan/enable_error_info.hpp"
 
 #include <sstream>
 #include <iomanip>
 
 #include <cstdio>
 
-#include <boost/filesystem/fstream.hpp>
 #include <boost/crc.hpp>
 
 namespace
@@ -15,20 +14,21 @@ namespace
     std::string crc32_checksum(const boost::filesystem::path &file)
     {
         boost::crc_32_type crc;
-        boost::filesystem::ifstream in(file, std::ios_base::binary);
-        if (!in.is_open())
-            BOOST_THROW_EXCEPTION(bunsan::system_error("open") << bunsan::error::message(file.string()));
-        char buf[BUFSIZ];
-        do
-        {
-            in.read(buf, BUFSIZ);
-            crc.process_bytes(buf, in.gcount());
-        }
-        while (in);
-        if (in.bad())
-            BOOST_THROW_EXCEPTION(bunsan::system_error("read") << bunsan::error::message(file.string()));
         std::stringstream out;
-        out<<std::hex<<std::uppercase<<crc.checksum();
+        BUNSAN_EXCEPTIONS_WRAP_BEGIN()
+        {
+            bunsan::filesystem::ifstream in(file, std::ios_base::binary);
+            char buf[BUFSIZ];
+            do
+            {
+                in.read(buf, BUFSIZ);
+                crc.process_bytes(buf, in.gcount());
+            }
+            while (in);
+            in.close();
+            out << std::hex << std::uppercase << crc.checksum();
+        }
+        BUNSAN_EXCEPTIONS_WRAP_END()
         return out.str();
     }
 }
@@ -41,25 +41,26 @@ namespace
     std::string CryptoPP_checksum(const boost::filesystem::path &file)
     {
         byte buf[BUFSIZ];
-        static_assert(sizeof(byte)==sizeof(char), "size of byte have to be equal size of char");
+        static_assert(sizeof(byte) == sizeof(char), "size of byte have to be equal size of char");
         HASH hash;
-        boost::filesystem::ifstream in(file, std::ios_base::binary);
-        if (!in.is_open())
-            BOOST_THROW_EXCEPTION(bunsan::system_error("open") << bunsan::error::message(file.string()));
-        do
-        {
-            in.read(reinterpret_cast<char *>(buf), BUFSIZ);
-            hash.Update(buf, in.gcount());
-        }
-        while (in);
-        if (in.bad())
-            BOOST_THROW_EXCEPTION(bunsan::system_error("read") << bunsan::error::message(file.string()));
-        byte out[HASH::DIGESTSIZE];
-        hash.Final(out);
         std::stringstream sout;
-        static_assert(sizeof(byte)<=sizeof(int), "size of byte have to be not greater than size of int");
-        for (byte i: out)
-            sout<<std::hex<<std::uppercase<<std::setw(2)<<std::setfill('0')<<int(i);
+        BUNSAN_EXCEPTIONS_WRAP_BEGIN()
+        {
+            bunsan::filesystem::ifstream in(file, std::ios_base::binary);
+            do
+            {
+                in.read(reinterpret_cast<char *>(buf), BUFSIZ);
+                hash.Update(buf, in.gcount());
+            }
+            while (in);
+            in.close();
+            byte out[HASH::DIGESTSIZE];
+            hash.Final(out);
+            static_assert(sizeof(byte) <= sizeof(int), "size of byte have to be not greater than size of int");
+            for (const byte i: out)
+                sout << std::hex << std::uppercase << std::setw(2) << std::setfill('0') << int(i);
+        }
+        BUNSAN_EXCEPTIONS_WRAP_END()
         return sout.str();
     }
 }
