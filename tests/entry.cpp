bf25fc0f8a293c598d473a417cea4ca1e35cbadb
Aleksey Filippov <sarum9in@gmail.com>
2012-12-12 15:46:26 +0400

    entry test suite refactoring.

--- tests/entry.cpp
+++ tests/entry.cpp
@@ -1,35 +1,33 @@
+#define BOOST_TEST_MODULE entry
+#include <boost/test/unit_test.hpp>
+
 #include "bunsan/pm/entry.hpp"
 
 #include <sstream>
-#include <cassert>
 
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/info_parser.hpp>
 
-int main()
+BOOST_AUTO_TEST_CASE(entry)
 {
-    using bunsan::pm::entry;
     std::string name = "some/long/name";
-    entry e(name);
-    assert(e.name()==name);
-    assert(e.location().string()==name);
-    assert(e.remote_resource("/repo", "res")=="/repo/"+name+"/res");
-    assert(e.remote_resource("repo", "res")=="repo/"+name+"/res");
-    assert(e.remote_resource("", "res")==name+"/res");
-    assert(e.remote_resource("repo")=="repo/"+name);
-    assert(e.remote_resource("")==name);
+    const bunsan::pm::entry e(name);
+    BOOST_CHECK_EQUAL(e.name(), name);
+    BOOST_CHECK_EQUAL(e.location().string(), name);
+    BOOST_CHECK_EQUAL(e.remote_resource("/repo", "res"), "/repo/" + name + "/res");
+    BOOST_CHECK_EQUAL(e.remote_resource("repo", "res"), "repo/" + name + "/res");
+    BOOST_CHECK_EQUAL(e.remote_resource("", "res"), name + "/res");
+    BOOST_CHECK_EQUAL(e.remote_resource("repo"), "repo/" + name);
+    BOOST_CHECK_EQUAL(e.remote_resource(""), name);
     boost::filesystem::path pname(name);
-    assert(e.local_resource("")==pname);
-    assert(e.local_resource("repo")=="repo"/pname);
-    assert(e.local_resource("/repo")=="/repo"/pname);
-    assert(e.local_resource("/repo", "o_O")=="/repo"/pname/"o_O");
-    assert((entry("123/456") / "789").name() == "123/456/789");
-    entry a("some//long/name");
-    assert(a==e);
-    using std::stringstream;
-    using boost::property_tree::read_info;
-    using boost::property_tree::ptree;
-    stringstream in(R"END(
+    BOOST_CHECK_EQUAL(e.local_resource(""), pname);
+    BOOST_CHECK_EQUAL(e.local_resource("repo"), "repo" / pname);
+    BOOST_CHECK_EQUAL(e.local_resource("/repo"), "/repo" / pname);
+    BOOST_CHECK_EQUAL(e.local_resource("/repo", "o_O"), "/repo" / pname / "o_O");
+    BOOST_CHECK_EQUAL((bunsan::pm::entry("123/456") / "789").name() ,  "123/456/789");
+    const bunsan::pm::entry a("some//long/name");
+    BOOST_CHECK_EQUAL(a, e);
+    std::stringstream in(R"END(
         some
         {
             long
@@ -37,7 +35,7 @@ int main()
                 name "hello world"
             }
         })END");
-    ptree pt;
-    read_info(in, pt);
-    assert(pt.get<std::string>(e.ptree_path())=="hello world");
+    boost::property_tree::ptree pt;
+    boost::property_tree::read_info(in, pt);
+    BOOST_CHECK_EQUAL(pt.get<std::string>(e.ptree_path()), "hello world");
 }
