cd7b7a5c9f9093bcc5da82a5cf3e2945a90e971c
Aleksey Filippov <sarum9in@gmail.com>
2012-12-18 03:08:49 +0400

    factory_options refactoring.

--- include/bunsan/utility/factory_options.hpp
+++ include/bunsan/utility/factory_options.hpp
@@ -8,8 +8,13 @@
 
 namespace bunsan{namespace utility
 {
+    template <typename Factory>
     struct factory_options
     {
+        typedef Factory factory_type;
+        typedef typename factory_type::bunsan_factory::result_type result_type;
+        typedef typename factory_type::bunsan_factory::arguments_size arguments_size;
+
         template <typename Archive>
         void serialize(Archive &ar, const unsigned int)
         {
@@ -18,10 +23,11 @@ namespace bunsan{namespace utility
         }
 
         /// Try to create instance of type using Factory, setup it on success.
-        template <typename Factory, typename ... Args>
-        typename Factory::bunsan_factory::result_type instance(Args &&...args) const
+        template <typename ... Args>
+        typename std::enable_if<arguments_size::value == sizeof...(Args),
+            result_type>::type instance(Args &&...args) const
         {
-            const typename Factory::bunsan_factory::result_type tmp = Factory::instance(type, std::forward<Args>(args)...);
+            const result_type tmp = factory_type::instance(type, std::forward<Args>(args)...);
             if (tmp)
                 tmp->setup(config);
             return tmp;
