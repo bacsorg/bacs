6bb445a9be07d541176de7178c325c905647a648
Aleksey Filippov <sarum9in@gmail.com>
2012-12-20 04:48:34 +0400

    problem namespace refactoring.

--- include/bacs/archive/problem.hpp
+++ include/bacs/archive/problem.hpp
@@ -2,41 +2,49 @@
 
 #include "bunsan/pm/entry.hpp"
 
-#include <boost/optional.hpp>
-
 #include <string>
 #include <vector>
-#include <set>
-#include <map>
+#include <unordered_set>
+#include <unordered_map>
+
+#include <boost/optional.hpp>
 
 namespace bacs{namespace archive{namespace problem
 {
     /// problem id
     typedef std::string id;
+
     typedef std::vector<unsigned char> binary;
-    /// information about problem, see problem.xsd from BACS.XSD
+
+    /// information about problem, see bacs::single::api::problem::Problem
     typedef binary info_type;
+
     /// package name
     typedef bunsan::pm::entry package;
+
     /// hash string
     typedef binary hash_type;
-    typedef std::set<id> id_set;
-    typedef std::map<id, info_type> info_map;
-    typedef std::map<id, hash_type> hash_map;
+
+    typedef std::unordered_set<id> id_set;
+    typedef std::unordered_map<id, info_type> info_map;
+    typedef std::unordered_map<id, hash_type> hash_map;
     typedef std::string flag_type;
-    typedef std::set<flag_type> flag_set;
-    struct status
+    typedef std::unordered_set<flag_type> flag_set;
+
+    struct status_type
     {
-        bool exists;
         flag_set flags;
         hash_type hash;
     };
-    typedef std::map<id, status> status_map;
+    typedef boost::optional<status_type> status;
+
+    typedef std::unordered_map<id, status> status_map;
+
     struct import_info
     {
-        bool ok;
-        std::string error;
+        boost::optional<std::string> error;
         status previous, current;
     };
-    typedef std::map<id, import_info> import_map;
+
+    typedef std::unordered_map<id, import_info> import_map;
 }}}
