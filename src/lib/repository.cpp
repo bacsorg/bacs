8e4333a293eece7af63cd22de0b3a91081d86df9
Aleksey Filippov <sarum9in@gmail.com>
2012-12-24 00:51:43 +0400

    repository: wrapper functions implementation.

--- src/lib/repository.cpp
+++ src/lib/repository.cpp
@@ -5,119 +5,151 @@
 
 #include "bunsan/utility/archiver.hpp"
 #include "bunsan/config/cast.hpp"
+#include "bunsan/filesystem/operations.hpp"
 
 #include <boost/filesystem/operations.hpp>
 
-using namespace bacs::archive;
-
-repository::repository(const boost::property_tree::ptree &ptree):
-    repository(bunsan::config::load<bacs::archive::config>(ptree)) {}
-
-repository::repository(const config &config_):
-    m_lock(boost::interprocess::open_or_create, config_.lock.c_str()),
-    m_resolver(config_.resolver),
-    m_tmpdir(config_.tmpdir),
-    m_problem_archiver(config_.problem.archiver.instance(m_resolver)) {}
-
-/* container */
-
-problem::import_map repository::insert_all(const archiver_options &archiver_options_, const boost::filesystem::path &archive)
+namespace bacs{namespace archive
 {
-    const bunsan::tempfile unpacked = bunsan::tempfile::in_dir(m_tmpdir);
-    const bunsan::utility::archiver_ptr archiver = archiver_options_.instance(m_resolver);
-    archiver->unpack(archive, unpacked.path());
-    problem::import_map map;
-    for (boost::filesystem::directory_iterator i(unpacked.path()), end; i != end; ++i)
+    repository::repository(const boost::property_tree::ptree &ptree):
+        repository(bunsan::config::load<bacs::archive::config>(ptree)) {}
+
+    repository::repository(const config &config_):
+        m_lock(boost::interprocess::open_or_create, config_.lock.c_str()),
+        m_resolver(config_.resolver),
+        m_tmpdir(config_.tmpdir),
+        m_problem_archiver(config_.problem.archiver.instance(m_resolver)) {}
+
+    namespace
     {
-        problem::id id = i->path().filename().string();
-        // TODO validate problem id (should be implemented in repository::insert)
-        map[id] = insert(id, i->path());
-    }
-    return map;
-}
-
-bunsan::tempfile repository::extract_all(const problem::id_set &id_set, const archiver_options &archiver_options_)
-{
-    // TODO validate problem id
-    const bunsan::tempfile unpacked = bunsan::tempfile::in_dir(m_tmpdir);
-    const bunsan::utility::archiver_ptr archiver = archiver_options_.instance(m_resolver);
-    for (const problem::id &id: id_set)
-    {
-        // ignore return value
-        extract(id, unpacked.path() / id);
-    }
-    bunsan::tempfile packed = bunsan::tempfile::in_dir(m_tmpdir);
-    archiver->pack_contents(packed.path(), unpacked.path());
-    return packed;
-}
-
-bool repository::available(const problem::id &id)
-{
-    return has_flag(id, problem::flag::ignore);
-}
-
-/* flags */
-
-problem::id_set repository::ignore_all(const problem::id_set &id_set)
-{
-    problem::id_set ret;
-    for (const auto &id: id_set)
-        if (ignore(id))
-            ret.insert(id);
-    return ret;
-}
-
-bool repository::ignore(const problem::id &id)
-{
-    return set_flag(id, problem::flag::ignore);
-}
-
-/* info */
-
-namespace
-{
-    template <typename Ret, typename RetMap, Ret (repository::*Getter)(const problem::id &)>
-    struct multiplex
-    {
-        template <typename T>
-        static inline void insert(RetMap &map, const problem::id &id, const T &ret)
+        template <typename Ret>
+        std::unordered_map<problem::id, Ret> get_all(repository *const this_,
+                                                     Ret (repository::*get)(const problem::id &),
+                                                     const problem::id_set &id_set)
         {
-            map[id] = ret;
-        }
-        template <typename T>
-        static inline void insert(RetMap &map, const problem::id &id, const boost::optional<T> &ret)
-        {
-            if (ret)
-                map[id] = ret.get();
-        }
-        static RetMap get(const problem::id_set &id_set, repository *this_)
-        {
-            RetMap map;
+            std::unordered_map<problem::id, Ret> map;
             for (const problem::id &id: id_set)
-                insert(map, id, (this_->*Getter)(id));
+                map[id] = (this_->*get)(id);
             return map;
         }
-    };
-}
 
-problem::info_map repository::info_all(const problem::id_set &id_set)
-{
-    return multiplex<boost::optional<problem::info_type>, problem::info_map, &repository::info>::get(id_set, this);
-}
+        problem::id_set get_all(repository *const this_,
+                                bool (repository::*get)(const problem::id &),
+                                const problem::id_set &id_set)
+        {
+            problem::id_set set;
+            for (const problem::id &id: id_set)
+                if ((this_->*get)(id))
+                    set.insert(id);
+            return set;
+        }
+    }
 
-problem::hash_map repository::hash_all(const problem::id_set &id_set)
-{
-    return multiplex<boost::optional<problem::hash_type>, problem::hash_map, &repository::hash>::get(id_set, this);
-}
+    /* container */
 
-problem::status_map repository::status_all(const problem::id_set &id_set)
-{
-    return multiplex<problem::status, problem::status_map, &repository::status>::get(id_set, this);
-}
+    problem::import_map repository::insert_all(const boost::filesystem::path &location)
+    {
+        problem::import_map map;
+        for (boost::filesystem::directory_iterator i(location), end; i != end; ++i)
+        {
+            const problem::id id = i->path().filename().string();
+            // TODO validate problem id (should be implemented in repository::insert)
+            map[id] = insert(id, i->path());
+        }
+        return map;
+    }
 
-/* repack */
+    problem::import_map repository::insert_all(const archiver_options &archiver_options_,
+                                               const boost::filesystem::path &archive)
+    {
+        const bunsan::tempfile unpacked = bunsan::tempfile::in_dir(m_tmpdir);
+        const bunsan::utility::archiver_ptr archiver = archiver_options_.instance(m_resolver);
+        archiver->unpack(archive, unpacked.path());
+        return insert_all(unpacked.path());
+    }
 
-problem::import_map repository::repack_all(const problem::id_set &id_set)
-{
-    return multiplex<problem::import_info, problem::import_map, &repository::repack>::get(id_set, this);
-}
+    void repository::extract_all(const problem::id_set &id_set,
+                                 const boost::filesystem::path &location)
+    {
+        bunsan::filesystem::reset_dir(location);
+        for (const problem::id &id: id_set)
+        {
+            const boost::filesystem::path dst = location / id;
+            if (!extract(id, dst) && boost::filesystem::exists(dst))
+                boost::filesystem::remove_all(dst);
+        }
+    }
+
+    void repository::extract_all(const problem::id_set &id_set,
+                                 const archiver_options &archiver_options_,
+                                 const boost::filesystem::path &archive)
+    {
+        const bunsan::tempfile unpacked = bunsan::tempfile::in_dir(m_tmpdir);
+        extract_all(id_set, unpacked.path());
+        archiver_options_.instance(m_resolver)->pack_contents(archive, unpacked.path());
+    }
+
+    bunsan::tempfile repository::extract_all(const problem::id_set &id_set,
+                                             const archiver_options &archiver_options_)
+    {
+        bunsan::tempfile packed = bunsan::tempfile::in_dir(m_tmpdir);
+        extract_all(id_set, archiver_options_, packed.path());
+        return packed;
+    }
+
+    problem::id_set repository::exists(const problem::id_set &id_set)
+    {
+        return get_all(this, &repository::exists, id_set);
+    }
+
+    problem::id_set repository::available(const problem::id_set &id_set)
+    {
+        return get_all(this, &repository::available, id_set);
+    }
+
+    bool repository::available(const problem::id &id)
+    {
+        return has_flag(id, problem::flag::ignore);
+    }
+
+    /* flags */
+
+    bool repository::ignore(const problem::id &id)
+    {
+        return set_flag(id, problem::flag::ignore);
+    }
+
+    problem::id_set repository::ignore_all(const problem::id_set &id_set)
+    {
+        return get_all(this, &repository::ignore, id_set);
+    }
+
+    problem::id_set repository::clear_flags_all(const problem::id_set &id_set)
+    {
+        return get_all(this, &repository::clear_flags, id_set);
+    }
+
+    /* info */
+
+    problem::status_map repository::status_all(const problem::id_set &id_set)
+    {
+        return get_all(this, &repository::status, id_set);
+    }
+
+    problem::info_map repository::info_all(const problem::id_set &id_set)
+    {
+        return get_all(this, &repository::info, id_set);
+    }
+
+    problem::hash_map repository::hash_all(const problem::id_set &id_set)
+    {
+        return get_all(this, &repository::hash, id_set);
+    }
+
+    /* repack */
+
+    problem::import_map repository::repack_all(const problem::id_set &id_set)
+    {
+        return get_all(this, &repository::repack, id_set);
+    }
+}}
