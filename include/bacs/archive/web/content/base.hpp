06eb8e99deed7f378a62060544c4bc285c1706f8
Aleksey Filippov <sarum9in@gmail.com>
2012-12-24 23:15:49 +0400

    base view of plain skin was implemented.

---
+++ include/bacs/archive/web/content/base.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <cppcms/view.h>
+
+#include <string>
+
+namespace bacs{namespace archive{namespace web{namespace content
+{
+    struct base: cppcms::base_content
+    {
+        std::string title;
+    };
+}}}}
