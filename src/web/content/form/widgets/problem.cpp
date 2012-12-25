7dbaa3fb19d23d459f39965d94fa6b9f725976a1
Aleksey Filippov <sarum9in@gmail.com>
2012-12-25 20:30:45 +0400

    web::content::form::widgets::problem: flag and flag_set implementation + id_set::value() bug fix.

--- src/web/content/form/widgets/problem.cpp
+++ src/web/content/form/widgets/problem.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <vector>
 
+#include <boost/algorithm/string/trim.hpp>
 #include <boost/algorithm/string/split.hpp>
 #include <boost/algorithm/string/join.hpp>
 #include <boost/algorithm/string/classification.hpp>
@@ -31,9 +32,11 @@ namespace bacs{namespace archive{namespace web{namespace content{namespace form{
     {
         archive::problem::id_set set;
         archive::problem::id value_(text::value());
-        boost::algorithm::split(set, value_,
-                                boost::algorithm::is_space(),
-                                boost::algorithm::token_compress_on);
+        boost::algorithm::trim(value_);
+        if (!value_.empty())
+            boost::algorithm::split(set, value_,
+                                    boost::algorithm::is_space(),
+                                    boost::algorithm::token_compress_on);
         return set;
     }
 
@@ -50,6 +53,34 @@ namespace bacs{namespace archive{namespace web{namespace content{namespace form{
             return false;
         const archive::problem::id_set set = value();
         valid(!set.empty() && std::all_of(set.begin(), set.end(), archive::problem::is_allowed_id));
+        return text::validate();
+    }
+
+    archive::problem::flag_set flag_set::value()
+    {
+        archive::problem::flag_set set;
+        archive::problem::flag value_(text::value());
+        boost::algorithm::trim(value_);
+        if (!value_.empty())
+            boost::algorithm::split(set, value_,
+                                    boost::algorithm::is_space(),
+                                    boost::algorithm::token_compress_on);
+        return set;
+    }
+
+    void flag_set::value(const archive::problem::flag_set &flag_set_)
+    {
+        std::vector<archive::problem::flag> flags(flag_set_.begin(), flag_set_.end());
+        std::sort(flags.begin(), flags.end());
+        text::value(boost::algorithm::join(flags, " "));
+    }
+
+    bool flag_set::validate()
+    {
+        if (!text::validate())
+            return false;
+        const archive::problem::flag_set set = value();
+        valid(std::all_of(set.begin(), set.end(), archive::problem::is_allowed_id));
         return text::validate();
     }
 }}}}}}}
