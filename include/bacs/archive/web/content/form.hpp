83fa0c582156a4c30556c596e769e680795e9abf
Aleksey Filippov <sarum9in@gmail.com>
2012-12-26 00:14:11 +0400

    web::content::{insert,extract,rename,status,clear_flags,ignore,info,hash,repack} implementation.

---
+++ include/bacs/archive/web/content/form.hpp
@@ -0,0 +1,74 @@
+#pragma once
+
+#include "bacs/archive/problem.hpp"
+
+#include "bacs/archive/web/content/base_form.hpp"
+
+#include "bacs/archive/web/content/form/list_query.hpp"
+#include "bacs/archive/web/content/form/insert.hpp"
+#include "bacs/archive/web/content/form/extract.hpp"
+#include "bacs/archive/web/content/form/rename.hpp"
+//#include "bacs/archive/web/content/form/existing.hpp"
+//#include "bacs/archive/web/content/form/available.hpp"
+// status
+//#include "bacs/archive/web/content/form/has_flag.hpp"
+//#include "bacs/archive/web/content/form/set_flags.hpp"
+//#include "bacs/archive/web/content/form/unset_flags.hpp"
+// clear_flags
+// ignore
+// info
+// hash
+// repack
+
+namespace bacs{namespace archive{namespace web{namespace content
+{
+    template <typename Form, typename Result>
+    struct basic_form: base_form
+    {
+        template <typename ... Args>
+        explicit basic_form(Args &&...args): form(std::forward<Args>(args)...) {}
+
+        Form form;
+        boost::optional<Result> result;
+    };
+
+    typedef basic_form<form::insert, problem::import_map> insert;
+    typedef basic_form<form::extract, std::string> extract;
+    typedef basic_form<form::rename, problem::import_info> rename;
+    // TODO existing
+    // TODO available
+
+    struct status: basic_form<form::list_query, problem::status_map>
+    {
+        status();
+    };
+
+    //typedef basic_form<form::has_flag, boost::optional<bool>> has_flag;
+    //typedef basic_form<form::set_flags, boost::optional<bool>> set_flags;
+    //typedef basic_form<form::unset_flags, boost::optional<bool>> unset_flags;
+
+    struct clear_flags: basic_form<form::list_query, problem::id_set>
+    {
+        clear_flags();
+    };
+
+    struct ignore: basic_form<form::list_query, problem::id_set>
+    {
+        ignore();
+    };
+
+    struct info: basic_form<form::list_query, problem::info_map>
+    {
+        info();
+    };
+
+    struct hash: basic_form<form::list_query, problem::hash_map>
+    {
+        hash();
+    };
+
+    struct repack: basic_form<form::list_query, problem::import_map>
+    {
+        repack();
+    };
+}}}}
---
+++ src/web/content/form.cpp
@@ -0,0 +1,19 @@
+#include "bacs/archive/web/content/form.hpp"
+
+namespace bacs{namespace archive{namespace web{namespace content
+{
+    // existing
+    // available
+
+    status::status(): basic_form(cppcms::locale::translate("Get status")) {}
+
+    clear_flags::clear_flags(): basic_form(cppcms::locale::translate("Clear flags")) {}
+
+    ignore::ignore(): basic_form(cppcms::locale::translate("Ignore")) {}
+
+    info::info(): basic_form(cppcms::locale::translate("Get info")) {}
+
+    hash::hash(): basic_form(cppcms::locale::translate("Get hash")) {}
+
+    repack::repack(): basic_form(cppcms::locale::translate("Repack")) {}
+}}}}
