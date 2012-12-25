4fb04f0f38c172d97d4721c21be9d17b20055a9a
Aleksey Filippov <sarum9in@gmail.com>
2012-12-25 23:01:11 +0400

    web::content::form::archiver_config label names fix.

--- src/web/content/form/archiver_config.cpp
+++ src/web/content/form/archiver_config.cpp
@@ -4,8 +4,8 @@ namespace bacs{namespace archive{namespace web{namespace content{namespace form
 {
     archiver_config::archiver_config()
     {
-        type.message(cppcms::locale::translate("Type"));
-        format.message(cppcms::locale::translate("Format"));
+        type.message(cppcms::locale::translate("Archive type"));
+        format.message(cppcms::locale::translate("Archive format"));
         add(type);
         add(format);
     }
