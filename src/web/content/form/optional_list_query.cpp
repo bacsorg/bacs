43f75113a1c25c82527235deb379e554ae9bdf4d
Aleksey Filippov <sarum9in@gmail.com>
2012-12-26 20:17:15 +0400

    web::content::form::optional_list_query improvement.
    ids shadowing when flag is selected.

--- src/web/content/form/optional_list_query.cpp
+++ src/web/content/form/optional_list_query.cpp
@@ -1,5 +1,8 @@
 #include "bacs/archive/web/content/form/optional_list_query.hpp"
 
+#include <random>
+#include <ctime>
+
 namespace bacs{namespace archive{namespace web{namespace content{namespace form
 {
     optional_list_query::optional_list_query(const std::string &flag_,
@@ -7,17 +10,27 @@ namespace bacs{namespace archive{namespace web{namespace content{namespace form
                                              const flag_type_enum flag_type_):
         flag_type(flag_type_)
     {
+        std::string ids_id(16, '\0');
+        {
+            std::mt19937 rng(std::time(nullptr));
+            std::uniform_int_distribution<char> rnd('a', 'z');
+            std::generate(ids_id.begin(), ids_id.end(), [&rng, &rnd](){return rnd(rng);});
+        }
         flag.name("flag");
         flag.message(flag_);
+        static const std::string jhead = R"EOF(onchange="document.getElementById(')EOF";
+        static const std::string jmid = R"EOF(').disabled = )EOF";
+        static const std::string jtail = R"EOF(this.checked;")EOF";
         switch (flag_type)
         {
         case flag_enables:
-            // TODO
+            flag.attributes_string(jhead + ids_id + jmid + "!" + jtail);
             break;
         case flag_disables:
-            // TODO
+            flag.attributes_string(jhead + ids_id + jmid + jtail);
             break;
         }
+        ids.id(ids_id);
         ids.name("ids");
         ids.message(cppcms::locale::translate("Problem ids"));
         submit.value(query);
