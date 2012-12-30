f23ebaf76fd900a4419a6260bc8bd54273a490b5
Aleksey Filippov <sarum9in@gmail.com>
2012-12-30 21:33:50 +0400

    web::content::form::with_flag implementation.

---
+++ include/bacs/archive/web/content/form/with_flag.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "bacs/archive/web/content/form/base.hpp"
+#include "bacs/archive/web/content/form/widgets/problem/optional_id_set.hpp"
+#include "bacs/archive/web/content/form/widgets/problem/flag.hpp"
+
+namespace bacs{namespace archive{namespace web{namespace content{namespace form
+{
+    struct with_flag: base
+    {
+        with_flag();
+
+        widgets::problem::optional_id_set ids;
+        widgets::problem::flag flag;
+        cppcms::widgets::submit submit;
+    };
+}}}}}
+
