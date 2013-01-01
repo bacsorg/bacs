5c875653aaae9a63881304ba6f82d499b873e46f
Aleksey Filippov <sarum9in@gmail.com>
2013-01-02 00:13:04 +0400

    web::content::error and error view of plain skin implementation.

---
+++ include/bacs/archive/web/content/error.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "bacs/archive/web/content/base.hpp"
+
+namespace bacs{namespace archive{namespace web{namespace content
+{
+    struct error: base
+    {
+        std::string brief;
+        std::string message;
+        bool raw = false; // Should it be rendered as raw html?
+    };
+}}}}
