c678cdb6bf372fb3bcb8f3fda8c25e1c6ba83ba1
Aleksey Filippov <sarum9in@gmail.com>
2013-01-02 00:19:03 +0400

    web::repository: protobuf response implementation + refactoring.

--- include/bacs/archive/web/repository.hpp
+++ include/bacs/archive/web/repository.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "bacs/archive/repository.hpp"
+#include "bacs/archive/web/content/form.hpp"
 
 #include <cppcms/application.h>
 
@@ -15,6 +16,9 @@ namespace bacs{namespace archive{namespace web
                    const std::shared_ptr<archive::repository> &repository_);
 
     private:
+        template <typename Model, typename Result, typename ProtoResult, bool (repository::*Handler)(Model &, Result &)>
+        void handler_wrapper(const std::string &name);
+
         void insert();
         void extract();
         void rename();
@@ -36,6 +40,22 @@ namespace bacs{namespace archive{namespace web
         void send_file(const boost::filesystem::path &path, const std::string &filename);
 
         void send_tempfile(bunsan::tempfile &&tmpfile, const std::string &filename);
+
+    private:
+        bool insert(content::insert &data, problem::import_map &result);
+        bool extract(content::extract &data, std::string &result);
+        bool rename(content::rename &data, problem::import_info &result);
+        bool existing(content::existing &data, problem::id_set &result);
+        bool available(content::available &data, problem::id_set &result);
+        bool status(content::status &data, problem::status_map &result);
+        bool with_flag(content::with_flag &data, problem::id_set &result);
+        bool set_flags(content::set_flags &data, problem::id_set &result);
+        bool unset_flags(content::unset_flags &data, problem::id_set &result);
+        bool clear_flags(content::clear_flags &data, problem::id_set &result);
+        bool ignore(content::ignore &data, problem::id_set &result);
+        bool info(content::info &data, problem::info_map &result);
+        bool hash(content::hash &data, problem::hash_map &result);
+        bool repack(content::repack &data, problem::import_map &result);
 
     private:
         const std::shared_ptr<archive::repository> m_repository;
--- src/web/repository.cpp
+++ src/web/repository.cpp
@@ -1,5 +1,7 @@
 #include "bacs/archive/web/repository.hpp"
 #include "bacs/archive/web/content/form.hpp"
+#include "bacs/archive/web/content/error.hpp"
+#include "bacs/archive/pb/convert.hpp"
 
 #include "bunsan/enable_error_info.hpp"
 #include "bunsan/filesystem/fstream.hpp"
@@ -61,20 +63,70 @@ namespace bacs{namespace archive{namespace web
         mapper().root("/repository");
     }
 
-    void repository::insert()
+    template <typename Model, typename Result, typename ProtoResult, bool (repository::*Handler)(Model &, Result &)>
+    void repository::handler_wrapper(const std::string &name)
     {
-        content::insert data;
+        bool do_render = true;
+        Model data;
         if (request().request_method() == "POST")
         {
             data.form.load(context());
             if (data.form.validate())
             {
-                const bunsan::tempfile tmpfile = bunsan::tempfile::unique(); // FIXME use specified dir from config
-                data.form.archive.value()->save_to(tmpfile.string());
-                data.result = m_repository->insert_all(data.form.config.value(), tmpfile.path());
+                Result result;
+                do_render = (this->*Handler)(data, result);
+                switch (data.form.response())
+                {
+                case content::form::base::response_type::html:
+                    data.result = result;
+                    break;
+                case content::form::base::response_type::protobuf:
+                    {
+                        ProtoResult proto_result;
+                        pb::convert(result, proto_result);
+                        send_protobuf(proto_result, name + ".pb.data");
+                        do_render = false;
+                    }
+                    break;
+                }
+            }
+            else
+            {
+                switch (data.form.response())
+                {
+                case content::form::base::response_type::html:
+                    // form::validate() results error messages set for form parts
+                    break;
+                case content::form::base::response_type::protobuf:
+                    {
+                        response().status(cppcms::http::response::internal_server_error, "Invalid request");
+                        content::error error;
+                        error.brief = translate("Invalid request.");
+                        error.message = translate("Form was not filled correctly.");
+                        render("error", error);
+                        do_render = false;
+                    }
+                    break;
+                }
             }
         }
-        render("insert", data);
+        if (do_render)
+            render(name, data);
+    }
+
+#define DEFINE_HANDLER(NAME, RESULT, PROTO_RESULT) \
+    void repository::NAME() \
+    { \
+        repository::handler_wrapper<content::NAME, RESULT, PROTO_RESULT, &repository::NAME>(#NAME); \
+    } \
+    bool repository::NAME(content::NAME &data, RESULT &result)
+
+    DEFINE_HANDLER(insert, problem::import_map, pb::problem::ImportMap)
+    {
+        const bunsan::tempfile tmpfile = bunsan::tempfile::unique(); // FIXME use specified dir from config
+        data.form.archive.value()->save_to(tmpfile.string());
+        result = m_repository->insert_all(data.form.config.value(), tmpfile.path());
+        return true;
     }
 
     void repository::extract()
@@ -94,184 +146,97 @@ namespace bacs{namespace archive{namespace web
         render("extract", data);
     }
 
-    void repository::rename()
+    /*DEFINE_HANDLER(extract, std::string, std::string)
     {
-        content::rename data;
-        if (request().request_method() == "POST")
-        {
-            data.form.load(context());
-            if (data.form.validate())
-            {
-                data.result = m_repository->rename(data.form.current.value(), data.form.future.value());
-            }
-        }
-        render("rename", data);
+        bunsan::tempfile tmpfile = m_repository->extract_all(data.form.ids.value(), data.form.config.value());
+        const std::string filename = "archive." + data.form.config.type.value() +
+                                     "." + data.form.config.format.value();
+        send_tempfile(std::move(tmpfile), filename);
+        return false;
+    }*/
+
+    DEFINE_HANDLER(rename, problem::import_info, pb::problem::ImportInfo)
+    {
+        result = m_repository->rename(data.form.current.value(), data.form.future.value());
+        return true;
     }
 
-    void repository::existing()
+    DEFINE_HANDLER(existing, problem::id_set, pb::problem::IdSet)
     {
-        content::existing data;
-        if (request().request_method() == "POST")
-        {
-            data.form.load(context());
-            if (data.form.validate())
-            {
-                const boost::optional<problem::id_set> ids = data.form.ids.value();
-                if (ids)
-                    data.result = m_repository->existing(ids.get());
-                else
-                    data.result = m_repository->existing();
-            }
-        }
-        render("existing", data);
+        const boost::optional<problem::id_set> ids = data.form.ids.value();
+        if (ids)
+            result = m_repository->existing(ids.get());
+        else
+            result = m_repository->existing();
+        return true;
     }
 
-    void repository::available()
+    DEFINE_HANDLER(available, problem::id_set, pb::problem::IdSet)
     {
-        content::available data;
-        if (request().request_method() == "POST")
-        {
-            data.form.load(context());
-            if (data.form.validate())
-            {
-                const boost::optional<problem::id_set> ids = data.form.ids.value();
-                if (ids)
-                    data.result = m_repository->available(ids.get());
-                else
-                    data.result = m_repository->available();
-            }
-        }
-        render("available", data);
+        const boost::optional<problem::id_set> ids = data.form.ids.value();
+        if (ids)
+            result = m_repository->available(ids.get());
+        else
+            result = m_repository->available();
+        return true;
     }
 
-    void repository::status()
+    DEFINE_HANDLER(status, problem::status_map, pb::problem::StatusMap)
     {
-        content::status data;
-        if (request().request_method() == "POST")
-        {
-            data.form.load(context());
-            if (data.form.validate())
-            {
-                data.result = m_repository->status_all(data.form.ids.value());
-            }
-        }
-        render("status", data);
+        result = m_repository->status_all(data.form.ids.value());
+        return true;
     }
 
-    void repository::with_flag()
+    DEFINE_HANDLER(with_flag, problem::id_set, pb::problem::IdSet)
     {
-        content::with_flag data;
-        if (request().request_method() == "POST")
-        {
-            data.form.load(context());
-            if (data.form.validate())
-            {
-                const boost::optional<problem::id_set> ids = data.form.ids.value();
-                if (ids)
-                    data.result = m_repository->with_flag(ids.get(), data.form.flag.value());
-                else
-                    data.result = m_repository->with_flag(data.form.flag.value());
-            }
-        }
-        render("with_flag", data);
+        const boost::optional<problem::id_set> ids = data.form.ids.value();
+        if (ids)
+            result = m_repository->with_flag(ids.get(), data.form.flag.value());
+        else
+            result = m_repository->with_flag(data.form.flag.value());
+        return true;
     }
 
-    void repository::set_flags()
+    DEFINE_HANDLER(set_flags, problem::id_set, pb::problem::IdSet)
     {
-        content::set_flags data;
-        if (request().request_method() == "POST")
-        {
-            data.form.load(context());
-            if (data.form.validate())
-            {
-                data.result = m_repository->set_flags_all(data.form.ids.value(), data.form.flags.value());
-            }
-        }
-        render("set_flags", data);
+        result = m_repository->set_flags_all(data.form.ids.value(), data.form.flags.value());
+        return true;
     }
 
-    void repository::unset_flags()
+    DEFINE_HANDLER(unset_flags, problem::id_set, pb::problem::IdSet)
     {
-        content::unset_flags data;
-        if (request().request_method() == "POST")
-        {
-            data.form.load(context());
-            if (data.form.validate())
-            {
-                data.result = m_repository->unset_flags_all(data.form.ids.value(), data.form.flags.value());
-            }
-        }
-        render("unset_flags", data);
+        result = m_repository->unset_flags_all(data.form.ids.value(), data.form.flags.value());
+        return true;
     }
 
-    void repository::clear_flags()
+    DEFINE_HANDLER(clear_flags, problem::id_set, pb::problem::IdSet)
     {
-        content::clear_flags data;
-        if (request().request_method() == "POST")
-        {
-            data.form.load(context());
-            if (data.form.validate())
-            {
-                data.result = m_repository->clear_flags_all(data.form.ids.value());
-            }
-        }
-        render("clear_flags", data);
+        result = m_repository->clear_flags_all(data.form.ids.value());
+        return true;
     }
 
-    void repository::ignore()
+    DEFINE_HANDLER(ignore, problem::id_set, pb::problem::IdSet)
     {
-        content::ignore data;
-        if (request().request_method() == "POST")
-        {
-            data.form.load(context());
-            if (data.form.validate())
-            {
-                data.result = m_repository->ignore_all(data.form.ids.value());
-            }
-        }
-        render("ignore", data);
+        result = m_repository->ignore_all(data.form.ids.value());
+        return true;
     }
 
-    void repository::info()
+    DEFINE_HANDLER(info, problem::info_map, pb::problem::InfoMap)
     {
-        content::info data;
-        if (request().request_method() == "POST")
-        {
-            data.form.load(context());
-            if (data.form.validate())
-            {
-                data.result = m_repository->info_all(data.form.ids.value());
-            }
-        }
-        render("info", data);
+        result = m_repository->info_all(data.form.ids.value());
+        return true;
     }
 
-    void repository::hash()
+    DEFINE_HANDLER(hash, problem::hash_map, pb::problem::HashMap)
     {
-        content::hash data;
-        if (request().request_method() == "POST")
-        {
-            data.form.load(context());
-            if (data.form.validate())
-            {
-                data.result = m_repository->hash_all(data.form.ids.value());
-            }
-        }
-        render("hash", data);
+        result = m_repository->hash_all(data.form.ids.value());
+        return true;
     }
 
-    void repository::repack()
+    DEFINE_HANDLER(repack, problem::import_map, pb::problem::ImportMap)
     {
-        content::repack data;
-        if (request().request_method() == "POST")
-        {
-            data.form.load(context());
-            if (data.form.validate())
-            {
-                data.result = m_repository->repack_all(data.form.ids.value());
-            }
-        }
-        render("repack", data);
+        result = m_repository->repack_all(data.form.ids.value());
+        return true;
     }
 
     template <typename ProtoBuf>
@@ -280,7 +245,7 @@ namespace bacs{namespace archive{namespace web
         response().content_type("application/octet-stream");
         BOOST_ASSERT(filename.find('"') == std::string::npos);
         response().set_header("Content-Disposition", "filename=\"" + filename + "\"");
-        protobuf.SerializeToOstream(response().out());
+        protobuf.SerializeToOstream(&response().out());
     }
 
     // FIXME note: these implementations are workarounds, use xsendfile and xsendtempfile instead
