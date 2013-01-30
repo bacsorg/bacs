a45583e5781bb4d251650c0b007b61b5584f625b
Aleksey Filippov <sarum9in@gmail.com>
2013-01-30 21:49:01 +0400

    CLI repack support + refactoring.

--- src/bin/cli.cpp
+++ src/bin/cli.cpp
@@ -14,29 +14,36 @@
 
 int main(int argc, char **argv)
 {
+    boost::program_options::options_description vdesc(argv[0]);
     std::string config_file;
+    std::string insert, insert_all;
     std::string format;
-    std::string input, output;
+    std::string output;
+    std::vector<std::string> arguments;
     try
     {
-        boost::program_options::options_description desc(argv[0]);
-        desc.add_options()
-            ("help,h", "Print this information")
+        vdesc.add_options()
             ("version,V", "Program version")
             ("config,c", boost::program_options::value<std::string>(&config_file))
             ("format,f", boost::program_options::value<std::string>(&format))
-            ("input,i", boost::program_options::value<std::string>(&input))
-            ("output,o", boost::program_options::value<std::string>(&output))
-            ("insert", "Insert problem from directory")
-            ("insert_all", "Insert all problems from archive");
+            ("output,o", boost::program_options::value<std::string>(&output), "Log file (stdout if not specified)")
+            ("insert", boost::program_options::value<std::string>(&insert), "Insert problem from directory")
+            ("insert_all", boost::program_options::value<std::string>(&insert_all), "Insert all problems from archive")
+            ("repack", "Insert all problems from archive");
+
+        boost::program_options::options_description desc(argv[0]);
+        desc.add(vdesc);
+
+        desc.add_options()
+            ("argument,a", boost::program_options::value<std::vector<std::string>>(&arguments)->composing(), "arguments");
+
+        boost::program_options::positional_options_description pdesc;
+        pdesc.add("argument", -1);
+
         boost::program_options::variables_map vm;
-        boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
+        boost::program_options::store(boost::program_options::command_line_parser(argc, argv).options(desc).positional(pdesc).run(), vm);
         boost::program_options::notify(vm);
-        if (vm.count("help"))
-        {
-            std::cerr << desc << std::endl;
-            return 1;
-        }
+
         if (vm.count("version"))
         {
             std::cerr << "It is too early to announce project version" << std::endl;
@@ -67,14 +74,14 @@ int main(int argc, char **argv)
             if (vm.count("insert_all"))
             {
                 const bacs::archive::problem::import_map map =
-                    archiver_options ? repo.insert_all(archiver_options.get(), input) : repo.insert_all(input);
+                    archiver_options ? repo.insert_all(archiver_options.get(), insert_all) : repo.insert_all(insert_all);
                 bacs::archive::pb::problem::ImportMap pb_map;
                 bacs::archive::pb::convert(map, pb_map);
                 *out << pb_map.DebugString() << std::flush;
             }
             else if (vm.count("insert"))
             {
-                const boost::filesystem::path path = boost::filesystem::absolute(input);
+                const boost::filesystem::path path = boost::filesystem::absolute(insert);
                 const bacs::archive::problem::import_info info = repo.insert(path.filename().string(), path);
                 bacs::archive::pb::problem::ImportInfo pb_info;
                 bacs::archive::pb::convert(info, pb_info);
@@ -92,10 +99,23 @@ int main(int argc, char **argv)
             {
                 // TODO
             }
+            else if (vm.count("repack"))
+            {
+                const bacs::archive::problem::id_set ids(arguments.begin(), arguments.end());
+                const bacs::archive::problem::import_map map = repo.repack_all(ids);
+                bacs::archive::pb::problem::ImportMap pb_map;
+                bacs::archive::pb::convert(map, pb_map);
+                *out << pb_map.DebugString() << std::flush;
+            }
             if (fout)
                 fout->close();
         }
         BUNSAN_EXCEPTIONS_WRAP_END()
+    }
+    catch (boost::program_options::error &e)
+    {
+        std::cerr << e.what() << "\n\n" << vdesc << std::endl;
+        return 200;
     }
     catch (std::exception &e)
     {
