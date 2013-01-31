18fea46c6d1051588683e583e9229cfa66a8bfbb
Aleksey Filippov <sarum9in@gmail.com>
2013-01-31 20:33:34 +0400

    protobuf_error, protobuf_parsing_error and protobuf_serialization_error declaration.

--- include/bacs/archive/error.hpp
+++ include/bacs/archive/error.hpp
@@ -8,6 +8,10 @@ namespace bacs{namespace archive
 {
     struct error: virtual bunsan::error {};
 
+    struct protobuf_error: virtual error {};
+    struct protobuf_parsing_error: virtual protobuf_error {};
+    struct protobuf_serialization_error: virtual protobuf_error {};
+
     struct problem_error: virtual error
     {
         typedef boost::error_info<struct tag_problem_id, problem::id> problem_id;
