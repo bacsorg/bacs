c24d32a410cb7202283327ed663925cfe1ecd564
Aleksey Filippov <sarum9in@gmail.com>
2013-05-20 09:13:11 +0400

    WEB implementation + build scripts.

---
+++ src/bin/web.cpp
@@ -0,0 +1,32 @@
+#include "bacs/statement_provider/web/server.hpp"
+
+#include <cppcms/applications_pool.h>
+#include <cppcms/service.h>
+
+#include <iostream>
+#include <memory>
+#include <cstdlib>
+
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/info_parser.hpp>
+
+int main(int argc, char *argv[])
+{
+    try
+    {
+        cppcms::service srv(argc, argv);
+        const std::string cfg = srv.settings().get<std::string>("statement_provider.pm.config");
+        boost::property_tree::ptree config;
+        boost::property_tree::read_info(cfg, config);
+        const std::shared_ptr<bunsan::pm::repository> repository(new bunsan::pm::repository(config));
+        srv.applications_pool().mount(
+            cppcms::applications_factory<bacs::statement_provider::web::server>(repository)
+        );
+        srv.run();
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
+}
