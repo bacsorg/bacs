7b457db4a6dc7282bd8fa3e9bae4c3d875e040c7
Aleksey Filippov <sarum9in@gmail.com>
2013-05-24 14:41:28 +0400

    cache implementation.

---
+++ include/bunsan/pm/cache.hpp
@@ -0,0 +1,116 @@
+#pragma once
+
+#include "bunsan/pm/repository.hpp"
+
+#include <boost/noncopyable.hpp>
+#include <boost/filesystem/path.hpp>
+#include <boost/thread/shared_mutex.hpp>
+#include <boost/serialization/access.hpp>
+#include <boost/serialization/nvp.hpp>
+
+namespace bunsan{namespace pm
+{
+    class cache: private boost::noncopyable
+    {
+    public:
+        struct config
+        {
+            template <typename Archive>
+            void serialize(Archive &ar, const unsigned int)
+            {
+                ar & BOOST_SERIALIZATION_NVP(cache.root);
+                ar & BOOST_SERIALIZATION_NVP(cache.lifetime);
+                ar & BOOST_SERIALIZATION_NVP(repository);
+            }
+
+            struct cache_config
+            {
+                boost::filesystem::path root;
+                std::time_t lifetime;
+            } cache;
+
+            pm::config repository;
+        };
+
+    public:
+        class entry;
+
+    public:
+        explicit cache(const config &config_);
+        explicit cache(const boost::property_tree::ptree &config_);
+
+        entry get(const pm::entry &package);
+
+        void clean();
+
+    private:
+        void clean_();
+
+    private:
+        bunsan::pm::repository m_repository;
+        const config::cache_config m_config;
+        boost::shared_mutex m_lock;
+    };
+
+    class cache::entry
+    {
+    public:
+        entry()=default;
+
+        inline entry(const entry &e): m_lock(e.m_lock), m_root(e.m_root)
+        {
+            lock();
+        }
+
+        inline entry &operator=(const entry &e)
+        {
+            entry tmp(e);
+            swap(tmp);
+            return *this;
+        }
+
+        inline entry(entry &&e)
+        {
+            swap(e);
+            e.unlock();
+        }
+
+        inline entry &operator=(entry &&e)
+        {
+            swap(e);
+            e.unlock();
+            return *this;
+        }
+
+        inline ~entry() { unlock(); }
+
+        inline explicit operator bool() const noexcept { return m_lock; }
+
+        const boost::filesystem::path &root() const;
+
+        inline void swap(entry &e) noexcept
+        {
+            using std::swap;
+
+            swap(m_lock, e.m_lock);
+            swap(m_root, e.m_root);
+        }
+
+    private:
+        friend class pm::cache;
+
+        entry(boost::shared_mutex *lock_,
+              const boost::filesystem::path &root);
+
+        void lock();
+        void unlock();
+
+    private:
+        boost::shared_mutex *m_lock = nullptr;
+        boost::filesystem::path m_root;
+    };
+    inline void swap(cache::entry &a, cache::entry &b) noexcept
+    {
+        a.swap(b);
+    }
+}}
---
+++ src/lib/cache.cpp
@@ -0,0 +1,92 @@
+#include "bunsan/pm/cache.hpp"
+
+#include "bunsan/config/cast.hpp"
+#include "bunsan/filesystem/operations.hpp"
+
+#include <boost/assert.hpp>
+#include <boost/thread/lock_types.hpp>
+
+namespace bunsan{namespace pm
+{
+    namespace
+    {
+        const boost::filesystem::path installation_subdir = ".install";
+        const boost::filesystem::path installation_data = "data";
+
+        pm::config update_config_data(pm::config config)
+        {
+            config.name.installation.data = installation_data;
+            return config;
+        }
+    }
+
+    cache::cache(const config &config_):
+        m_repository(update_config_data(config_.repository)),
+        m_config(config_.cache)
+    {
+        clean_();
+    }
+
+    cache::cache(const boost::property_tree::ptree &config_):
+        cache(bunsan::config::load<cache::config>(config_)) {}
+
+    cache::entry cache::get(const pm::entry &package)
+    {
+        const boost::filesystem::path root = m_config.root / package.location() / installation_subdir;
+        if (m_repository.need_update(package, root, m_config.lifetime))
+        {
+            const boost::unique_lock<boost::shared_mutex> lk(m_lock);
+            const boost::filesystem::file_status status = boost::filesystem::symlink_status(root);
+            switch (status.type())
+            {
+            default:
+                clean_();
+            case boost::filesystem::file_not_found:
+                boost::filesystem::create_directories(root);
+            case boost::filesystem::directory_file:
+                m_repository.update(package, root, m_config.lifetime);
+            }
+        }
+        return entry(&m_lock, root / installation_data);
+    }
+
+    void cache::clean()
+    {
+        const boost::unique_lock<boost::shared_mutex> lk(m_lock);
+        clean_();
+    }
+
+    void cache::clean_()
+    {
+        // requires lock
+        bunsan::filesystem::reset_dir(m_config.root);
+    }
+
+    const boost::filesystem::path &cache::entry::root() const
+    {
+        BOOST_ASSERT(*this);
+        return m_root;
+    }
+
+    cache::entry::entry(boost::shared_mutex *lock_,
+                        const boost::filesystem::path &root):
+        m_lock(lock_),
+        m_root(root)
+    {
+        BOOST_ASSERT(*this);
+        lock();
+    }
+
+    void cache::entry::lock()
+    {
+        if (*this)
+            m_lock->lock_shared();
+    }
+
+    void cache::entry::unlock()
+    {
+        if (m_lock)
+            m_lock->unlock_shared();
+        m_lock = nullptr;
+    }
+}}
