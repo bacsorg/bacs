4394af0a42b3d0cb192f14932d60718852d4e585
Aleksey Filippov <sarum9in@gmail.com>
2013-05-30 17:30:58 +0400

    resource::parse::{time_millis,memory_bytes}() implementation.

---
+++ include/bacs/problem/resource/error.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "bacs/problem/error.hpp"
+
+namespace bacs{namespace problem{namespace resource
+{
+    struct error: virtual problem::error {};
+}}}
---
+++ include/bacs/problem/resource/parse.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "bacs/problem/resource/error.hpp"
+
+#include <string>
+
+namespace bacs{namespace problem{namespace resource{namespace parse
+{
+    struct error: virtual resource::error
+    {
+        typedef boost::error_info<struct tag_value, std::string> value;
+        typedef boost::error_info<struct tag_pos, std::size_t> pos;
+    };
+
+    struct time_error: virtual error {};
+    struct memory_error: virtual error {};
+
+    std::uint64_t time_millis(const std::string &time);
+    std::uint64_t memory_bytes(const std::string &memory);
+}}}}
