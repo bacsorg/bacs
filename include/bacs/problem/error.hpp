21b3f10a622825d9a48f062a8121e35b0eec7f76
Aleksey Filippov <sarum9in@gmail.com>
2013-05-30 10:03:34 +0400

    statement, statement_versions::{copy,pdflatex} implementation.

--- include/bacs/problem/error.hpp
+++ include/bacs/problem/error.hpp
@@ -19,4 +19,16 @@ namespace bacs{namespace problem
     {
         typedef boost::error_info<struct tag_id, problem::id> id;
     };
+
+    struct statement_error: virtual error {};
+
+    struct invalid_statement_lang_error: virtual invalid_id_error, virtual statement_error
+    {
+        typedef boost::error_info<struct tag_lang, std::string> lang;
+    };
+
+    struct invalid_statement_format_error: virtual invalid_id_error, virtual statement_error
+    {
+        typedef boost::error_info<struct tag_format, std::string> format;
+    };
 }}
---
+++ src/statement_versions/copy.hpp
@@ -0,0 +1,35 @@
+#include "bacs/problem/statement.hpp"
+#include "bacs/problem/error.hpp"
+
+#include <boost/filesystem/path.hpp>
+
+#include <string>
+
+namespace bacs{namespace problem{namespace statement_versions
+{
+    struct copy_error: virtual error {};
+
+    struct invalid_source_name_error: virtual copy_error
+    {
+        typedef boost::error_info<struct tag_source_name, boost::filesystem::path> source_name;
+    };
+
+    class copy: public statement::version
+    {
+    public:
+        copy(const boost::filesystem::path &location,
+             const boost::property_tree::ptree &config);
+
+        void make_package(const boost::filesystem::path &destination,
+                          const bunsan::pm::entry &package,
+                          const bunsan::pm::entry &resources_package) const override;
+
+        pb::problem::Statement::Version info() const override;
+
+    private:
+        const boost::filesystem::path m_source;
+
+    private:
+        static const bool factory_reg_hook;
+    };
+}}}
