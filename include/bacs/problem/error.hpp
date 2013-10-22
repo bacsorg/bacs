415d38fd14ef92e8597454ae2999f3a18df0c6f9
Aleksey Filippov <sarum9in@gmail.com>
2013-10-22 13:12:38 +0400

    error::path declaration.

--- include/bacs/problem/error.hpp
+++ include/bacs/problem/error.hpp
@@ -3,12 +3,16 @@
 #include <bacs/problem/common.hpp>
 
 #include <bunsan/error.hpp>
+#include <bunsan/filesystem/error.hpp>
 
 #include <boost/filesystem/path.hpp>
 
 namespace bacs{namespace problem
 {
-    struct error: virtual bunsan::error {};
+    struct error: virtual bunsan::error
+    {
+        typedef bunsan::filesystem::error::path path;
+    };
 
     struct problem_format_error: virtual error
     {
