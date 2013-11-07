699ec5e94735fe54e3b07ef5771c83db6168f4cd
Aleksey Filippov <sarum9in@gmail.com>
2013-11-07 04:52:14 +0400

    importer::convert() refactoring.

--- src/lib/importer.cpp
+++ src/lib/importer.cpp
@@ -1,17 +1,28 @@
 #include <bacs/archive/importer.hpp>
 
+#include <bacs/archive/error.hpp>
+
 namespace bacs{namespace archive
 {
     importer::importer(const boost::property_tree::ptree &config):
         m_config(config) {}
 
-    problem::Problem importer::convert(const options &options_)
+    bacs::problem::Problem importer::convert(const options &options_)
     {
-        if (!m_impl.get())
+        try
         {
-            const importer_ptr tmp = instance(m_config);
-            m_impl.reset(new importer_ptr(tmp));
+            if (!m_impl.get())
+            {
+                const importer_ptr tmp = instance(m_config);
+                m_impl.reset(new importer_ptr(tmp));
+            }
+            return (*m_impl)->convert(options_);
         }
-        return (*m_impl)->convert(options_);
+        catch (std::exception &)
+        {
+            BOOST_THROW_EXCEPTION(bacs::problem::importer_convert_error() <<
+                                  bacs::problem::importer_convert_error::options(options_) <<
+                                  bunsan::enable_nested_current());
+        }
     }
 }}
