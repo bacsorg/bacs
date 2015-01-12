2a4ac27020c79f6bb6d25e3ce84bc4418ebdb6d4
Aleksey Filippov <sarum9in@gmail.com>
2015-01-12 11:19:52 +0100

    Integration with bunsan::application.

--- src/bin/cli.cpp
+++ src/bin/cli.cpp
@@ -1,108 +1,152 @@
 #include <bunsan/pm/repository.hpp>
 
+#include <bunsan/application.hpp>
 #include <bunsan/logging/legacy.hpp>
 #include <bunsan/property_tree/info_parser.hpp>
 
-#include <boost/program_options.hpp>
 #include <boost/property_tree/ptree.hpp>
 
 #include <exception>
 #include <iostream>
 
-int main(int argc, char **argv)
+using namespace bunsan::application;
+
+namespace
 {
-    std::string config_file;
-    std::string extract_to;
-    std::string install_to;
-    std::string update_installation;
-    std::string package;
-    std::string create;
-    std::string create_recursively;
-    std::time_t lifetime;
-    try
+    class pm_application: public application
     {
-        // command line parse
-        boost::program_options::options_description desc(argv[0]);
-        desc.add_options()
-            ("help,h", "Print this information")
-            ("version,V", "Program version")
-            ("config,c", boost::program_options::value<std::string>(&config_file)->default_value("pm.rc"), "Configuration file")
+    public:
+        using application::application;
+
+        void initialize_argument_parser(argument_parser &parser) override
+        {
+            application::initialize_argument_parser(parser);
+            parser.add_options()
+            (
+                "config,c",
+                value<std::string>(&config_file)->default_value("pm.rc"),
+                "Configuration file"
+            )
             ("clean", "Clean cache")
-            ("package,p", boost::program_options::value<std::string>(&package), "Package name")
-            ("extract,e", boost::program_options::value<std::string>(&extract_to), "Extract package to location")
-            ("install,i", boost::program_options::value<std::string>(&install_to), "Install package to location")
-            ("update,u", boost::program_options::value<std::string>(&update_installation), "Update installed package")
-            ("lifetime,l", boost::program_options::value<std::time_t>(&lifetime), "Only check for update if lifetime has passed since previous attempt")
-            ("create,r", boost::program_options::value<std::string>(&create), "Create source package from source")
-            ("create-recursively,R", boost::program_options::value<std::string>(&create_recursively), "Create source package from source")
+            (
+                "package,p",
+                value<std::string>(&package),
+                "Package name"
+            )
+            (
+                "extract,e",
+                value<std::string>(&extract_to),
+                "Extract package to location"
+            )
+            (
+                "install,i",
+                value<std::string>(&install_to),
+                "Install package to location"
+            )
+            (
+                "update,u",
+                value<std::string>(&update_installation),
+                "Update installed package"
+            )
+            (
+                "lifetime,l",
+                value<std::time_t>(&lifetime),
+                "Only check for update if lifetime "
+                "has passed since previous attempt"
+            )
+            (
+                "create,r",
+                value<std::string>(&create),
+                "Create source package from source"
+            )
+            (
+                "create-recursively,R",
+                value<std::string>(&create_recursively),
+                "Create source package from source"
+            )
             ("initialize,I", "Initialize cache")
             ("strip,s", "Strip source package from excess files.");
-        boost::program_options::variables_map vm;
-        boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
-        boost::program_options::notify(vm);
-        if (vm.count("help"))
-        {
-            std::cerr << desc << std::endl;
-            return 1;
         }
-        if (vm.count("version"))
+
+        void print_version() override
         {
             std::cerr << bunsan::pm::repository::version() << std::endl;
-            return 0;
         }
-        DLOG(config parse);
-        boost::property_tree::ptree config;
-        bunsan::property_tree::read_info(config_file, config);
-        if (vm.count("initialize"))
-        { // initialize before repository construction
-            std::cerr << "Attempt to initialize repository" << std::endl;
-            bunsan::pm::repository::initialize_cache(config);
-        }
-        bunsan::pm::repository repo(config);
-        if (vm.count("clean"))
-        { // clean before working with packages
-            std::cerr << "Attempt to clean repository" << std::endl;
-            repo.clean_cache();
-        }
-        if (vm.count("package"))
+
+        int main(const variables_map &variables) override
         {
-            if (vm.count("extract"))
-            { // extracting
-                std::cerr << "Attempt to extract \"" << package << "\" to \"" << extract_to << "\"" << std::endl;
-                repo.extract(package, extract_to);
+            DLOG(config parse);
+            boost::property_tree::ptree config;
+            bunsan::property_tree::read_info(config_file, config);
+            if (variables.count("initialize"))
+            { // initialize before repository construction
+                std::cerr << "Attempt to initialize repository" << std::endl;
+                bunsan::pm::repository::initialize_cache(config);
             }
-            else if (vm.count("install"))
-            {
-                repo.install(package, install_to);
+            bunsan::pm::repository repo(config);
+            if (variables.count("clean"))
+            { // clean before working with packages
+                std::cerr << "Attempt to clean repository" << std::endl;
+                repo.clean_cache();
             }
-            else if (vm.count("update"))
+            if (variables.count("package"))
             {
-                if (vm.count("lifetime"))
-                    repo.update(package, update_installation, lifetime);
+                if (variables.count("extract"))
+                { // extracting
+                    std::cerr << "Attempt to extract \"" << package << "\""
+                                 " to \"" << extract_to << "\"" << std::endl;
+                    repo.extract(package, extract_to);
+                }
+                else if (variables.count("install"))
+                {
+                    repo.install(package, install_to);
+                }
+                else if (variables.count("update"))
+                {
+                    if (variables.count("lifetime"))
+                        repo.update(package, update_installation, lifetime);
+                    else
+                        repo.update(package, update_installation);
+                }
                 else
-                    repo.update(package, update_installation);
+                { // package info
+                    std::cerr << "Package \"" << package << "\"" << std::endl;
+                }
             }
-            else
-            { // package info
-                std::cerr << "Package \"" << package << "\"" << std::endl;
+            else if (variables.count("create"))
+            {
+                std::cerr << "Attempt to create source package "
+                             "from source \"" << create << "\"" << std::endl;
+                repo.create(create, variables.count("strip"));
             }
+            else if (variables.count("create-recursively"))
+            {
+                std::cerr << "Attempt to create source packages recursively "
+                             "from source starting from \"" <<
+                             create_recursively << "\"" << std::endl;
+                repo.create_recursively(
+                    create_recursively,
+                    variables.count("strip")
+                );
+            }
+            return exit_success;
         }
-        else if (vm.count("create"))
-        {
-            std::cerr << "Attempt to create source package from source \"" << create << "\"" << std::endl;
-            repo.create(create, vm.count("strip"));
-        }
-        else if (vm.count("create-recursively"))
-        {
-            std::cerr << "Attempt to create source packages recursively from source starting from \"" << create_recursively << "\"" << std::endl;
-            repo.create_recursively(create_recursively, vm.count("strip"));
-        }
-        // end parse
-    }
-    catch (std::exception &e)
-    {
-        DLOG(Oops! An exception has occured);
-        std::cerr << e.what() << std::endl;
-        return 200;
-    }
+
+    private:
+        std::string config_file;
+        std::string extract_to;
+        std::string install_to;
+        std::string update_installation;
+        std::string package;
+        std::string create;
+        std::string create_recursively;
+        std::time_t lifetime;
+    };
+}
+
+int main(int argc, char **argv)
+{
+    pm_application app(argc, argv);
+    app.name("bunsan::pm::cli");
+    return app.exec();
 }
