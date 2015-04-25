40d94abf8c840ad3006dc59e2dda4944f5d68022
Aleksey Filippov <sarum9in@gmail.com>
2015-04-25 12:42:21 +0200

    Added project stub.

---
+++ Bunsan.Broker/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bunsan.Broker
+{
+    public class Class1
+    {
+    }
+}
