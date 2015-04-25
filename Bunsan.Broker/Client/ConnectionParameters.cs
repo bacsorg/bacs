6f1256dd925ab883c898f19c67d240d427336bde
Aleksey Filippov <sarum9in@gmail.com>
2015-04-25 13:51:30 +0200

    Added Credentials and Client.ConnectionParameters.

---
+++ Bunsan.Broker/Client/ConnectionParameters.cs
@@ -0,0 +1,22 @@
+﻿using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bunsan.Broker.Client
+{
+    [ProtoContract]
+    class ConnectionParameters
+    {
+        [ProtoMember(1, IsRequired = false)]
+        public string identifier { get; set; }
+
+        [ProtoMember(2, IsRequired = false)]
+        public string connection { get; set; }
+
+        [ProtoMember(3, IsRequired = false)]
+        public Credentials credentials { get; set; }
+    }
+}
---
+++ Bunsan.Broker/Credentials.cs
@@ -0,0 +1,28 @@
+﻿using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bunsan.Broker
+{
+    [ProtoContract]
+    class Credentials
+    {
+        [ProtoMember(1, IsRequired = false)]
+        public string method { get; set; }
+
+        [ProtoMember(2, IsRequired = false)]
+        public string username { get; set; }
+
+        [ProtoMember(3, IsRequired = false)]
+        public string password { get; set; }
+
+        [ProtoMember(4, IsRequired = false)]
+        public byte[] certificate { get; set; }
+
+        [ProtoMember(5, IsRequired = false)]
+        public byte[] key { get; set; }
+    }
+}
