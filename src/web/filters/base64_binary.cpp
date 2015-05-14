704ef677c6a87a9b1dfd78d81256204c42f7cf86
Aleksey Filippov <sarum9in@gmail.com>
2015-05-15 00:14:52 +0200

    Integrated with bunsan::crypto.

--- src/web/filters/base64_binary.cpp
+++ src/web/filters/base64_binary.cpp
@@ -1,11 +1,11 @@
 #include <bacs/archive/web/filters/base64_binary.hpp>
 
-#include <bunsan/base64.hpp>
+#include <bunsan/crypto/base64.hpp>
 
 namespace cppcms{namespace filters
 {
     void base64_binary::operator()(std::ostream &out) const
     {
-        out << bunsan::base64::encode(m_binary);
+        out << bunsan::crypto::base64::encode(m_binary);
     }
 }}
