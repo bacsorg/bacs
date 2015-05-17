1cf4f11ccea9fae81d6a27495abf950fc164a945
Aleksey Filippov <sarum9in@gmail.com>
2015-05-18 01:35:42 +0200

    Added mock_{archiver,builder,fetcher,maker,utility}.

---
+++ include/bunsan/utility/mock_maker.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <bunsan/utility/maker.hpp>
+
+#include <turtle/mock.hpp>
+
+namespace bunsan{namespace utility
+{
+    MOCK_BASE_CLASS(mock_maker, maker)
+    {
+        MOCK_METHOD(exec, 2,
+                    void (const boost::filesystem::path &cwd,
+                          const std::vector<std::string> &targets))
+    };
+}}
