79d32b90b6a275c35cae28fe35c8c83b9586b777
Aleksey Filippov <sarum9in@gmail.com>
2015-05-19 23:13:29 +0200

    Refactored CLI.

--- src/bin/cli.cpp
+++ src/bin/cli.cpp
@@ -2,9 +2,9 @@
 #include <bacs/archive/pb/problem.pb.h>
 #include <bacs/archive/repository.hpp>
 
-#include <bunsan/enable_error_info.hpp>
+#include <bunsan/application.hpp>
 #include <bunsan/filesystem/fstream.hpp>
-#include <bunsan/logging/legacy.hpp>
+#include <bunsan/logging/trivial.hpp>
 #include <bunsan/property_tree/info_parser.hpp>
 
 #include <boost/program_options.hpp>
@@ -12,102 +12,131 @@
 
 #include <memory>
 
-int main(int argc, char **argv)
+using namespace bunsan::application;
+
+namespace
 {
-    boost::program_options::options_description vdesc(argv[0]);
-    std::string config_file;
-    std::string insert, insert_all;
-    std::string format;
-    std::vector<std::string> arguments;
-    try
+    class archive_application: public application
     {
-        vdesc.add_options()
-            ("version,V", "Program version")
-            ("config,c", boost::program_options::value<std::string>(&config_file))
-            ("format,f", boost::program_options::value<std::string>(&format))
-            ("insert", boost::program_options::value<std::string>(&insert), "Insert problem from directory")
-            ("insert_all", boost::program_options::value<std::string>(&insert_all), "Insert all problems from archive")
-            ("repack", "Insert all problems from archive");
+    public:
+        using application::application;
 
-        boost::program_options::options_description desc(argv[0]);
-        desc.add(vdesc);
-
-        desc.add_options()
-            ("argument,a", boost::program_options::value<std::vector<std::string>>(&arguments)->composing(), "arguments");
-
-        boost::program_options::positional_options_description pdesc;
-        pdesc.add("argument", -1);
-
-        boost::program_options::variables_map vm;
-        boost::program_options::store(boost::program_options::command_line_parser(argc, argv).options(desc).positional(pdesc).run(), vm);
-        boost::program_options::notify(vm);
-
-        if (vm.count("version"))
+        void initialize_argument_parser(argument_parser &parser) override
         {
-            std::cerr << "It is too early to announce project version" << std::endl;
-            return 1;
+            application::initialize_argument_parser(parser);
+            parser.add_positional(
+                "argument",
+                -1,
+                value<std::vector<std::string>>(&arguments)
+            ).add_options()
+            (
+                "config,c",
+                value<std::string>(&config_file)
+            )
+            (
+                "format,f",
+                value<std::string>(&format)
+            )
+            (
+                "insert",
+                value<std::string>(&insert),
+                "Insert problem from directory"
+            )
+            (
+                "insert_all",
+                value<std::string>(&insert_all),
+                "Insert all problems from archive"
+            )
+            (
+                "repack",
+                "Insert all problems from archive"
+            );
         }
-        DLOG(config parse);
-        boost::property_tree::ptree config;
-        bunsan::property_tree::read_info(config_file, config);
-        bacs::archive::repository repo(config);
-        boost::optional<bacs::archive::archiver_options> archiver_options;
-        if (vm.count("format"))
+
+        int main(const variables_map &variables) override
         {
-            archiver_options = bacs::archive::archiver_options();
-            const std::string::size_type pos = format.find(':');
-            archiver_options->type = format.substr(0, pos);
-            if (pos != std::string::npos)
-                archiver_options->config.put("format", format.substr(pos + 1));
-        }
-        {
-            if (vm.count("insert_all"))
+            BUNSAN_LOG_DEBUG << "Parsing configuration at \""
+                             << config_file << "\"";
+            boost::property_tree::ptree config;
+            bunsan::property_tree::read_info(config_file, config);
+            bacs::archive::repository repo(config);
+            boost::optional<bacs::archive::archiver_options> archiver_options;
+            if (variables.count("format"))
             {
+                archiver_options = bacs::archive::archiver_options();
+                const std::string::size_type pos = format.find(':');
+                archiver_options->type = format.substr(0, pos);
+                if (pos != std::string::npos)
+                    archiver_options->config.put("format",
+                                                 format.substr(pos + 1));
+            }
+            if (variables.count("insert_all"))
+            {
+                BUNSAN_LOG_INFO << "Inserting all problems";
                 const bacs::archive::problem::import_map map =
-                    archiver_options ? repo.insert_all(archiver_options.get(), insert_all) : repo.insert_all(insert_all);
+                    archiver_options ?
+                        repo.insert_all(archiver_options.get(), insert_all) :
+                        repo.insert_all(insert_all);
                 bacs::archive::pb::problem::ImportMap pb_map;
                 bacs::archive::pb::convert(map, pb_map);
-                std::cout << pb_map.DebugString() << std::flush;
+                BUNSAN_LOG_TRACE << pb_map.DebugString();
             }
-            else if (vm.count("insert"))
+            else if (variables.count("insert"))
             {
-                const boost::filesystem::path path = boost::filesystem::absolute(insert);
-                const bacs::archive::problem::import_info info = repo.insert(path.filename().string(), path);
+                const boost::filesystem::path path =
+                    boost::filesystem::absolute(insert);
+                BUNSAN_LOG_INFO << "Inserting " << path;
+                const bacs::archive::problem::import_info info =
+                    repo.insert(path.filename().string(), path);
                 bacs::archive::pb::problem::ImportInfo pb_info;
                 bacs::archive::pb::convert(info, pb_info);
-                std::cout << pb_info.DebugString() << std::flush;
+                BUNSAN_LOG_TRACE << pb_info.DebugString();
             }
-            else if (vm.count("extract"))
+            else if (variables.count("extract"))
             {
                 // TODO
+                BUNSAN_LOG_WARNING << "Extract is not implemented!";
+                return exit_failure;
             }
-            else if (vm.count("info"))
+            else if (variables.count("info"))
             {
                 // TODO
+                BUNSAN_LOG_WARNING << "Info is not implemented!";
+                return exit_failure;
             }
-            else if (vm.count("hash"))
+            else if (variables.count("hash"))
             {
                 // TODO
+                BUNSAN_LOG_WARNING << "Hash is not implemented!";
+                return exit_failure;
             }
-            else if (vm.count("repack"))
+            else if (variables.count("repack"))
             {
-                const bacs::archive::problem::id_set ids(arguments.begin(), arguments.end());
-                const bacs::archive::problem::import_map map = repo.repack_all(ids);
+                BUNSAN_LOG_INFO << "Repacking";
+                const bacs::archive::problem::id_set ids(
+                    arguments.begin(),
+                    arguments.end()
+                );
+                const bacs::archive::problem::import_map map =
+                    repo.repack_all(ids);
                 bacs::archive::pb::problem::ImportMap pb_map;
                 bacs::archive::pb::convert(map, pb_map);
-                std::cout << pb_map.DebugString() << std::flush;
+                BUNSAN_LOG_TRACE << pb_map.DebugString();
             }
+            return exit_success;
         }
-    }
-    catch (boost::program_options::error &e)
-    {
-        std::cerr << e.what() << "\n\n" << vdesc << std::endl;
-        return 200;
-    }
-    catch (std::exception &e)
-    {
-        DLOG(Oops! An exception has occured);
-        std::cerr << e.what() << std::endl;
-        return 200;
-    }
+
+    private:
+        std::string config_file;
+        std::string insert, insert_all;
+        std::string format;
+        std::vector<std::string> arguments;
+    };
+}
+
+int main(int argc, char **argv)
+{
+    archive_application app(argc, argv);
+    app.name("bacs::archive::cli");
+    return app.exec();
 }
