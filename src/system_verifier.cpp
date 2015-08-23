6e08cca796e9492160590601f9c1b4be020e82a9
Aleksey Filippov <sarum9in@gmail.com>
2015-08-24 02:52:05 +0400

    Fixed system_verifier::make_package().

--- src/system_verifier.cpp
+++ src/system_verifier.cpp
@@ -16,8 +16,8 @@ bool system_verifier::make_package(const boost::filesystem::path &destination,
   index.source.import.source.insert(
       std::make_pair(".", "bacs/system/system_verifier"));
   index.source.self.insert(std::make_pair(".", "src"));
-  boost::filesystem::create_directory(destination / "src");
-  bunsan::filesystem::ofstream fout(destination / "src" /
+  boost::filesystem::create_directories(destination / "src" / "src");
+  bunsan::filesystem::ofstream fout(destination / "src" / "src" /
                                     "system_verifier.cpp");
   BUNSAN_FILESYSTEM_FSTREAM_WRAP_BEGIN(fout) {
     fout << "#include <bacs/system/system_verifier.hpp>\n"
@@ -25,18 +25,25 @@ bool system_verifier::make_package(const boost::filesystem::path &destination,
          << "#include <bunsan/protobuf/binary.hpp>\n"
          << "\n"
          << "namespace bacs {\n"
-         << "namespace problem {\n"
+         << "namespace system {\n"
          << "\n"
          << "class proto_system_verifier : public system_verifier {\n"
          << " public:\n"
          << "  proto_system_verifier();\n"
          << "  bool verify(const problem::System &request,\n"
          << "              problem::SystemResult &result) override;\n"
+         << "  static system_verifier_uptr make_instance() {\n"
+         << "    return std::make_unique<proto_system_verifier>();\n"
+         << "  }\n"
          << "\n"
          << " private:\n"
          << "   const problem::System m_system;\n"
-         << "   static const char system_encoded[];\n"
+         << "   static const std::string system_encoded;\n"
          << "};\n"
+         << "\n"
+         << "BUNSAN_PLUGIN_AUTO_REGISTER(\n"
+         << "    system_verifier, proto_system_verifier,\n"
+         << "    proto_system_verifier::make_instance)\n"
          << "\n"
          << "proto_system_verifier::proto_system_verifier()\n"
          << "    : m_system(bunsan::protobuf::binary::parse_make<\n"
@@ -53,7 +60,7 @@ bool system_verifier::make_package(const boost::filesystem::path &destination,
          << "  return true;\n"
          << "}\n"
          << "\n"
-         << "const char proto_system_verifier::system_encoded[] = {\n";
+         << "const std::string proto_system_verifier::system_encoded = {\n";
     const std::string system_encoded =
         bunsan::protobuf::binary::to_string(m_system);
     for (const int c : system_encoded) {
@@ -62,10 +69,11 @@ bool system_verifier::make_package(const boost::filesystem::path &destination,
     fout << "};\n"
          << "\n"
          << "\n"
-         << "}  // namespace problem\n"
+         << "}  // namespace system\n"
          << "}  // namespace bacs\n";
   } BUNSAN_FILESYSTEM_FSTREAM_WRAP_END(fout)
   fout.close();
+  index.save(destination / "index");
   return true;
 }
 
