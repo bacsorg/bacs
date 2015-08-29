f20515315a0dbd958abb1159f97ea0b520ec73a6
Aleksey Filippov <sarum9in@gmail.com>
2015-08-29 20:59:27 +0400

    Added ENABLE_WEB build option.

--- src/bin/server.cpp
+++ src/bin/server.cpp
@@ -1,22 +1,31 @@
 #include <bacs/archive/archive_service.hpp>
 #include <bacs/archive/repository.hpp>
+#ifdef BACS_ARCHIVE_USE_WEB
 #include <bacs/archive/web/repository.hpp>
+#endif
 
 #include <bunsan/application.hpp>
 #include <bunsan/filesystem/operations.hpp>
 #include <bunsan/log/trivial.hpp>
 #include <bunsan/protobuf/text.hpp>
+#ifdef BACS_ARCHIVE_USE_WEB
 #include <bunsan/web/json.hpp>
+#endif
 
+#ifdef BACS_ARCHIVE_USE_WEB
 #include <booster/aio/io_service.h>
 #include <cppcms/applications_pool.h>
 #include <cppcms/service.h>
+#endif
 
 #include <grpc++/server.h>
 #include <grpc++/server_builder.h>
 #include <grpc++/server_credentials.h>
 
 #include <boost/asio/io_service.hpp>
+#ifndef BACS_ARCHIVE_USE_WEB
+#include <boost/asio/signal_set.hpp>
+#endif
 #include <boost/property_tree/info_parser.hpp>
 #include <boost/property_tree/ptree.hpp>
 
@@ -39,10 +48,14 @@ class archive_application : public application {
     )(
         "rpc-configuration,r", value<std::string>(&rpc_config_path)->required(),
         "Configuration of RPC service"
-    )(
+    )
+#ifdef BACS_ARCHIVE_USE_WEB
+    (
         "web-configuration,w", value<std::string>(&web_config_path)->required(),
         "Configuration of Web backend"
-    );
+    )
+#endif
+    ;
   }
 
   auto make_repository(boost::asio::io_service &executor) {
@@ -83,6 +96,7 @@ class archive_application : public application {
     return builder.BuildAndStart();
   }
 
+#ifdef BACS_ARCHIVE_USE_WEB
   auto make_web_server(const std::shared_ptr<repository> &repository) {
     BUNSAN_LOG_DEBUG << "Parsing Web configuration at \"" << web_config_path
                      << "\"";
@@ -92,6 +106,7 @@ class archive_application : public application {
         cppcms::applications_factory<web::repository>(repository));
     return std::move(srv);
   }
+#endif
 
   int main(const variables_map & /*variables*/) override {
     boost::asio::io_service executor;
@@ -99,14 +114,28 @@ class archive_application : public application {
     const auto repository = make_repository(executor);
     const auto rpc_service = make_rpc_service(repository);
     const auto rpc_server = make_rpc_server(*rpc_service);
+#ifdef BACS_ARCHIVE_USE_WEB
     const auto web_server = make_web_server(repository);
+#endif
 
-    std::thread worker([&executor] { executor.run(); });
     boost::asio::io_service::work work(executor);
+    const auto terminate = [&] {
+      rpc_server->Shutdown();
+      executor.stop();
+    };
+#ifdef BACS_ARCHIVE_USE_WEB
+    std::thread worker([&executor] { executor.run(); });
     web_server->run();  // handles signals
-    rpc_server->Shutdown();
-    executor.stop();
+    terminate();
     worker.join();
+#else
+    boost::asio::signal_set signals(executor, SIGINT, SIGTERM);
+    signals.async_wait(
+        [&](const boost::system::error_code &ec, const int /*signum*/) {
+          terminate();
+        });
+    executor.run();
+#endif
     rpc_server->Wait();
     BUNSAN_LOG_INFO << "Server terminated";
     return exit_success;
@@ -119,8 +148,10 @@ class archive_application : public application {
   std::string rpc_config_path;
   ArchiveServerConfig rpc_config;
 
+#ifdef BACS_ARCHIVE_USE_WEB
   std::string web_config_path;
   cppcms::json::value web_config;
+#endif
 };
 }  // namespace
 
