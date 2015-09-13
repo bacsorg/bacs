a7fb5a5da2277738332391ecd39c9f8224e4a772
Aleksey Filippov <sarum9in@gmail.com>
2015-09-13 20:56:37 +0400

    Added authorize function support to BUNSAN_RPC_IMPLEMENT_*().

--- include/bunsan/rpc/implement.hpp
+++ include/bunsan/rpc/implement.hpp
@@ -8,8 +8,16 @@ namespace bunsan {
 namespace rpc {
 namespace detail {
 
-template <typename F>
-grpc::Status implement_status(F &&f) {
+template <typename Authorize, typename F>
+grpc::Status implement_status(Authorize &&authorize, F &&f) {
+  try {
+    const grpc::Status auth = authorize();
+    if (!auth.ok()) return auth;
+  } catch (std::exception &e) {
+    return grpc::Status(grpc::UNKNOWN, "Unable to authorize");
+  } catch (...) {
+    return grpc::Status(grpc::UNKNOWN, "Unable to authorize");
+  }
   try {
     return f();
   } catch (std::exception &e) {
@@ -19,9 +27,10 @@ grpc::Status implement_status(F &&f) {
   }
 }
 
-template <typename Response, typename F>
-grpc::Status implement_response(Response &&response, F &&f) {
-  return implement_status([response, &f] {
+template <typename Authorize, typename Response, typename F>
+grpc::Status implement_response(Authorize &&authorize, Response &&response,
+                                F &&f) {
+  return implement_status(authorize, [response, &f] {
     *response = f();
     return grpc::Status::OK;
   });
@@ -31,11 +40,19 @@ grpc::Status implement_response(Response &&response, F &&f) {
 }  // namespace bunsan
 }  // namespace bunsan
 
-#define BUNSAN_RPC_IMPLEMENT_STATUS(CONTEXT, REQUEST, RESPONSE, BODY) \
-  return ::bunsan::rpc::detail::implement_status(                     \
+#define BUNSAN_RPC_IMPLEMENT_STATUS(CONTEXT, REQUEST, RESPONSE, AUTHORIZE, \
+                                    BODY)                                  \
+  return ::bunsan::rpc::detail::implement_status(                          \
+      [this, CONTEXT, REQUEST, RESPONSE] {                                 \
+    return AUTHORIZE(CONTEXT, REQUEST, RESPONSE);                          \
+      },                                                                   \
       [this, CONTEXT, REQUEST, RESPONSE]() -> ::grpc::Status BODY);
 
-#define BUNSAN_RPC_IMPLEMENT_RESPONSE(CONTEXT, REQUEST, RESPONSE, BODY) \
-  return ::bunsan::rpc::detail::implement_response(                     \
-      RESPONSE, [this, CONTEXT, REQUEST]()                              \
+#define BUNSAN_RPC_IMPLEMENT_RESPONSE(CONTEXT, REQUEST, RESPONSE, AUTHORIZE, \
+                                      BODY)                                  \
+  return ::bunsan::rpc::detail::implement_response(                          \
+      [this, CONTEXT, REQUEST, RESPONSE] {                                   \
+    return AUTHORIZE(CONTEXT, REQUEST, RESPONSE);                            \
+      },                                                                     \
+      RESPONSE, [this, CONTEXT, REQUEST]()                                   \
                     -> std::remove_reference_t<decltype(*RESPONSE)> BODY);
--- tests/implement.cpp
+++ tests/implement.cpp
@@ -6,38 +6,78 @@
 BOOST_AUTO_TEST_SUITE(implement)
 
 struct service {
-  grpc::Status call0(int *context, const int *request, int *response) {
-    BUNSAN_RPC_IMPLEMENT_STATUS(context, request, response, {
+  grpc::Status call0(int *const context, const int *request, int *response) {
+    BUNSAN_RPC_IMPLEMENT_STATUS(context, request, response, authorize, {
       BOOST_CHECK_EQUAL(*context, 10);
       BOOST_CHECK_EQUAL(*request, 20);
       *response = 30;
       return grpc::Status::CANCELLED;
     })
   }
-  grpc::Status call1(int *context, const int *request, int *response) {
-    BUNSAN_RPC_IMPLEMENT_RESPONSE(context, request, response, {
+  grpc::Status call1(int *const context, const int *request, int *response) {
+    BUNSAN_RPC_IMPLEMENT_RESPONSE(context, request, response, authorize, {
       BOOST_CHECK_EQUAL(*context, 40);
       BOOST_CHECK_EQUAL(*request, 50);
       return 60;
     })
   }
   grpc::Status call2(int *context, const int *request, int *response) {
-    BUNSAN_RPC_IMPLEMENT_STATUS(context, request, response, {
+    BUNSAN_RPC_IMPLEMENT_STATUS(context, request, response, authorize, {
       throw std::runtime_error("hello error world");
     })
+  }
+  grpc::Status call3(int *context, const int *request, int *response) {
+    BUNSAN_RPC_IMPLEMENT_STATUS(context, request, response, authorize, {
+      BOOST_ERROR("Unreachable");
+      return grpc::Status::OK;
+    })
+  }
+  grpc::Status call4(int *context, const int *request, int *response) {
+    BUNSAN_RPC_IMPLEMENT_RESPONSE(context, request, response, authorize, {
+      BOOST_ERROR("Unreachable");
+      return 0;
+    })
+  }
+  template <typename Request, typename Response>
+  grpc::Status authorize(int *const context, Request * /*request*/,
+                         Response * /*response*/) {
+    if (*context < 0) {
+      return grpc::Status(grpc::PERMISSION_DENIED, "Permission denied");
+    }
+    return grpc::Status::OK;
   }
 };
 
 BOOST_AUTO_TEST_CASE(calls) {
-  service s;
-  int c0 = 10, q0 = 20, r0, c1 = 40, q1 = 50, r1, c2 = 70, q2 = 80, r2;
-  BOOST_CHECK_EQUAL(s.call0(&c0, &q0, &r0).error_code(), grpc::CANCELLED);
-  BOOST_CHECK_EQUAL(r0, 30);
-  BOOST_CHECK(s.call1(&c1, &q1, &r1).ok());
-  BOOST_CHECK_EQUAL(r1, 60);
-  const grpc::Status s2 = s.call2(&c2, &q2, &r2);
-  BOOST_CHECK_EQUAL(s2.error_code(), grpc::UNKNOWN);
-  BOOST_CHECK_EQUAL(s2.error_message(), "hello error world");
+  service srv;
+  {
+    int c = 10, q = 20, r;
+    BOOST_CHECK_EQUAL(srv.call0(&c, &q, &r).error_code(), grpc::CANCELLED);
+    BOOST_CHECK_EQUAL(r, 30);
+  }
+  {
+    int c = 40, q = 50, r;
+    BOOST_CHECK(srv.call1(&c, &q, &r).ok());
+    BOOST_CHECK_EQUAL(r, 60);
+  }
+  {
+    int c = 70, q = 80, r;
+    const grpc::Status s = srv.call2(&c, &q, &r);
+    BOOST_CHECK_EQUAL(s.error_code(), grpc::UNKNOWN);
+    BOOST_CHECK_EQUAL(s.error_message(), "hello error world");
+  }
+  {
+    int c = -30, q = -20, r;
+    const grpc::Status s = srv.call3(&c, &q, &r);
+    BOOST_CHECK_EQUAL(s.error_code(), grpc::PERMISSION_DENIED);
+    BOOST_CHECK_EQUAL(s.error_message(), "Permission denied");
+  }
+  {
+    int c = -60, q = -50, r;
+    const grpc::Status s = srv.call4(&c, &q, &r);
+    BOOST_CHECK_EQUAL(s.error_code(), grpc::PERMISSION_DENIED);
+    BOOST_CHECK_EQUAL(s.error_message(), "Permission denied");
+  }
 }
 
 BOOST_AUTO_TEST_SUITE_END()  // implement
