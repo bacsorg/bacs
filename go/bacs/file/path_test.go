f5885c233863c8f831e20daf26e87a0f61caeb50
Aleksey Filippov <sarum9in@gmail.com>
2015-09-24 15:59:35 +0400

    Added bacs_file.Path.GoPath().

---
+++ go/bacs/file/path_test.go
@@ -0,0 +1,40 @@
+package bacs_file
+
+import (
+    "github.com/stretchr/testify/assert"
+    "testing"
+)
+
+func TestGoPath(t *testing.T) {
+    testData := []struct {
+        p        Path
+        expected string
+    }{
+        {
+            Path{},
+            "",
+        },
+        {
+            Path{
+                Element: []string{
+                    "hello",
+                    "world",
+                },
+            },
+            "hello/world",
+        },
+        {
+            Path{
+                Root: "/",
+                Element: []string{
+                    "hello",
+                    "world",
+                },
+            },
+            "/hello/world",
+        },
+    }
+    for _, tt := range testData {
+        assert.Equal(t, tt.expected, tt.p.GoPath())
+    }
+}
