bb6139a4ea85d0ef101f10faab5a1c8e86190315
Aleksey Filippov <sarum9in@gmail.com>
2015-11-01 22:29:18 +0400

    Added worker.WorkerPool.

---
+++ go/bunsan/broker/worker/worker_pool.go
@@ -0,0 +1,86 @@
+package worker
+
+import (
+	"log"
+	"sync"
+
+	"github.com/bunsanorg/broker/go/bunsan/broker/service"
+)
+
+type WorkerPool interface {
+	// Add new worker, can be called any time.
+	Add(worker Worker) error
+	// Process requests from channel.
+	DoAll(requests <-chan service.Request) error
+	// Finish last request gracefully, do not process new requests.
+	Cancel() error
+	// Abort everything now.
+	Abort() error
+}
+
+type workerPool struct {
+	workersWaitGroup sync.WaitGroup
+	workers          []Worker
+	requests         chan service.Request
+	canceler         chan struct{}
+}
+
+func (p *workerPool) run(worker Worker) {
+	for request := range p.requests {
+		err := worker.Do(request)
+		if err != nil {
+			log.Print(err)
+		}
+	}
+	p.workersWaitGroup.Done()
+}
+
+func (p *workerPool) Add(worker Worker) error {
+	p.workers = append(p.workers, worker)
+	p.workersWaitGroup.Add(1)
+	go p.run(worker)
+	return nil
+}
+
+func (p *workerPool) DoAll(requests <-chan service.Request) error {
+	for run := true; run; {
+		select {
+		case request, ok := <-requests:
+			if ok {
+				select {
+				case p.requests <- request:
+					// just continue
+				case <-p.canceler:
+					run = false
+				}
+			} else {
+				run = false
+			}
+		case <-p.canceler:
+			run = false
+		}
+	}
+	close(p.requests)
+	p.workersWaitGroup.Wait()
+	return nil
+}
+
+func (p *workerPool) Cancel() error {
+	close(p.requests)
+	return nil
+}
+
+func (p *workerPool) Abort() error {
+	err := p.Cancel()
+	if err != nil {
+		return err
+	}
+	for _, worker := range p.workers {
+		werr := worker.Abort()
+		if werr != nil {
+			// FIXME returns some error
+			err = werr
+		}
+	}
+	return err
+}
