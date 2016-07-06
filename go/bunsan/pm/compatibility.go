203572f5e0a2c3c1d22d57f1b37a933e6a2e5fec
Aleksey Filippov <sarum9in@gmail.com>
2016-07-06 19:10:28 +0400

    Added missing methods to Repository.

--- go/bunsan/pm/compatibility.go
+++ go/bunsan/pm/compatibility.go
@@ -6,6 +6,7 @@ package pm
 import "C"
 
 import (
+	"time"
 	"unsafe"
 )
 
@@ -56,10 +57,13 @@ func (r *cRepository) Create(path string, strip bool) error {
 	return nil
 }
 
-func (r *cRepository) CleanCache() error {
+func (r *cRepository) CreateRecursively(rootPath string, strip bool) error {
+	cRootPath := C.CString(rootPath)
+	defer C.free(unsafe.Pointer(cRootPath))
 	cErr := C.malloc(errSize)
 	defer C.free(unsafe.Pointer(cErr))
-	cRet := C.bunsan_pm_repository_clean_cache(r.repo, (*C.char)(cErr), errSize)
+	cRet := C.bunsan_pm_repository_create_recursively(r.repo, cRootPath,
+		C.bool(strip), (*C.char)(cErr), errSize)
 	if cRet != 0 {
 		return &cError{C.GoString((*C.char)(cErr))}
 	}
@@ -75,6 +79,80 @@ func (r *cRepository) Extract(pkg, destination string) error {
 	defer C.free(cErr)
 	cRet := C.bunsan_pm_repository_extract(
 		r.repo, cPkg, cDst, (*C.char)(cErr), errSize)
+	if cRet != 0 {
+		return &cError{C.GoString((*C.char)(cErr))}
+	}
+	return nil
+}
+
+func (r *cRepository) Install(pkg, destination string) error {
+	cPkg := C.CString(pkg)
+	defer C.free(unsafe.Pointer(cPkg))
+	cDst := C.CString(destination)
+	defer C.free(unsafe.Pointer(cDst))
+	cErr := C.malloc(errSize)
+	defer C.free(cErr)
+	cRet := C.bunsan_pm_repository_install(r.repo, cPkg, cDst,
+		(*C.char)(cErr), errSize)
+	if cRet != 0 {
+		return &cError{C.GoString((*C.char)(cErr))}
+	}
+	return nil
+}
+
+func (r *cRepository) ForceUpdate(pkg, destination string) error {
+	cPkg := C.CString(pkg)
+	defer C.free(unsafe.Pointer(cPkg))
+	cDst := C.CString(destination)
+	defer C.free(unsafe.Pointer(cDst))
+	cErr := C.malloc(errSize)
+	defer C.free(cErr)
+	cRet := C.bunsan_pm_repository_force_update(r.repo, cPkg, cDst,
+		(*C.char)(cErr), errSize)
+	if cRet != 0 {
+		return &cError{C.GoString((*C.char)(cErr))}
+	}
+	return nil
+}
+
+func (r *cRepository) Update(pkg, destination string, lifetime time.Time) error {
+	cPkg := C.CString(pkg)
+	defer C.free(unsafe.Pointer(cPkg))
+	cDst := C.CString(destination)
+	defer C.free(unsafe.Pointer(cDst))
+	cErr := C.malloc(errSize)
+	defer C.free(cErr)
+	cRet := C.bunsan_pm_repository_update(r.repo, cPkg, cDst,
+		C.time_t(lifetime.Unix()), (*C.char)(cErr), errSize)
+	if cRet != 0 {
+		return &cError{C.GoString((*C.char)(cErr))}
+	}
+	return nil
+}
+
+func (r *cRepository) NeedUpdate(pkg, destination string, lifetime time.Time) (
+	bool, error) {
+
+	cPkg := C.CString(pkg)
+	defer C.free(unsafe.Pointer(cPkg))
+	cDst := C.CString(destination)
+	defer C.free(unsafe.Pointer(cDst))
+	cErr := C.malloc(errSize)
+	defer C.free(cErr)
+	var cNeed C._Bool
+	cRet := C.bunsan_pm_repository_need_update(r.repo, cPkg, cDst,
+		C.time_t(lifetime.Unix()), &cNeed,
+		(*C.char)(cErr), errSize)
+	if cRet != 0 {
+		return bool(cNeed), &cError{C.GoString((*C.char)(cErr))}
+	}
+	return bool(cNeed), nil
+}
+
+func (r *cRepository) CleanCache() error {
+	cErr := C.malloc(errSize)
+	defer C.free(unsafe.Pointer(cErr))
+	cRet := C.bunsan_pm_repository_clean_cache(r.repo, (*C.char)(cErr), errSize)
 	if cRet != 0 {
 		return &cError{C.GoString((*C.char)(cErr))}
 	}
--- go/bunsan/pm/repository.go
+++ go/bunsan/pm/repository.go
@@ -1,10 +1,17 @@
 package pm
 
+import "time"
+
 //go:generate bunsan-mockgen -gofile=$GOFILE
 
 type Repository interface {
-	Create(path string, strip bool) error
-	CleanCache() error
+	Create(sourcePath string, strip bool) error
+	CreateRecursively(rootPath string, strip bool) error
 	Extract(pkg, destination string) error
+	Install(pkg, destination string) error
+	ForceUpdate(pkg, destination string) error
+	Update(pkg, destination string, lifetime time.Time) error
+	NeedUpdate(pkg, destination string, lifetime time.Time) (bool, error)
+	CleanCache() error
 	Close() error
 }
