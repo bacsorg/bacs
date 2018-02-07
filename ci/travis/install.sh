726d69522a6cd3cb92d2a03b5f8c12fb65236e82
Aleksey Filippov <sarum9in@gmail.com>
2018-02-07 00:42:45 +0000

    Install boost from Ubuntu Bionic repository.

--- ci/travis/install.sh
+++ ci/travis/install.sh
@@ -16,6 +16,11 @@ boost_src="https://dl.bintray.com/boostorg/release/${boost_ver}/source/boost_$(e
 boost_sha256='bd0df411efd9a585e5a2212275f8762079fed8842264954675a4fddc46cfcf60'
 boost_dir="$(basename "$boost_src" .tar.gz)"
 
+# TODO checksums
+boost_ver='1.65.1'
+boost_repo='http://cz.archive.ubuntu.com'
+boost_repo_dir="$boost_repo/ubuntu/pool/main/b/boost$boost_ver"
+
 turtle_ver='1.3.0'
 turtle_src="http://downloads.sourceforge.net/project/turtle/turtle/$turtle_ver/turtle-${turtle_ver}.tar.bz2"
 turtle_sha256='1f0a8f7b7862e0f99f3849d60b488b1ce6546f1f7cfeb4d8f6c0261f1e3dcbe0'
@@ -57,6 +62,46 @@ function install_boost() (
   run sudo ./b2 install | egrep -v '^common\.copy'
 )
 
+function unpack() {
+  local name="$1"
+  local fname="$(basename "$name")"
+  wget "$name"
+  ar x "$fname"
+  sudo tar xf "data.tar.xz" -C /
+  sudo ldconfig
+}
+
+function install_boost_bin() (
+  binsuffix="${boost_ver}_${boost_ver}+dfsg-0ubuntu4_amd64.deb"
+  devsuffix="${boost_ver%.*}-dev_$boost_ver+dfsg-0ubuntu4_amd64.deb"
+  bd_libs=(atomic chrono date-time filesystem program-options serialization system thread)
+  b_libs=(context coroutine iostreams python random regex)
+  mkdir boost
+  cd boost
+  for i in "${bd_libs[@]}"
+  do
+    run unpack "$boost_repo_dir/libboost-$i$binsuffix"
+    run unpack "$boost_repo_dir/libboost-$i$devsuffix"
+  done
+  for i in "${b_libs[@]}"
+  do
+    run unpack "$boost_repo_dir/libboost-$i$binsuffix"
+  done
+  run unpack "$boost_repo_dir/libboost$devsuffix"
+  #for i in "${bd_libs[@]}" "${b_libs[@]}"
+  #do
+  #  ln -s 
+  #done
+  sudo ln -sf /usr/lib/x86_64-linux-gnu/libboost_*.so* /usr/lib
+  sudo ldconfig
+  #run ldd /usr/lib/x86_64-linux-gnu/libboost_filesystem.so
+  run ldd /usr/lib/x86_64-linux-gnu/libboost_*.so* || true
+  run ldd /usr/lib/libboost_*.so* || true
+  #run ls -l /usr/lib/x86_64-linux-gnu/libboost_*
+  #run ls -l /usr/lib/x86_64-linux-gnu/libstdc++.so.6
+  #run find /usr/lib
+)
+
 function install_turtle() (
   run wget "$turtle_src"
   run sha256verify "$(basename "$turtle_src")" "$turtle_sha256"
@@ -76,6 +121,7 @@ function install_botan() (
 
 fold install_meson
 fold install_ninja
-fold install_boost
+#fold install_boost
+fold install_boost_bin
 fold install_turtle
 fold install_botan
