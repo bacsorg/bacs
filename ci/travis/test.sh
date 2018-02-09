76f5d311ac3c152d636d66d3d70008ebaffe092c
Aleksey Filippov <sarum9in@gmail.com>
2018-02-09 00:30:44 +0000

    Use meson from PATH.

--- ci/travis/test.sh
+++ ci/travis/test.sh
@@ -2,7 +2,6 @@
 
 source "$(dirname "$0")/travis.sh"
 
-# FIXME use system version of meson
-run python3.6 ./meson/meson.py builddir
+run meson builddir
 run ninja -C builddir -j"$JOBS"
 run ninja -C builddir test
