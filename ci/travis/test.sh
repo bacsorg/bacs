7adb978cc2c3b2d421c15186f5b7c035b5aa4afe
Aleksey Filippov <sarum9in@gmail.com>
2018-02-13 19:09:47 +0000

    Added CMake testing to travis.

--- ci/travis/test.sh
+++ ci/travis/test.sh
@@ -2,6 +2,27 @@
 
 source "$(dirname "$0")/travis.sh"
 
-run meson builddir
-run ninja -C builddir -j"$JOBS"
-run ninja -C builddir test
+function gen_cmake() (
+  mkdir "$1"
+  cd "$1"
+  shift
+  cmake .. "$@"
+)
+
+case "$BUILD" in
+  meson)
+      run meson builddir
+      run ninja -C builddir -j"$JOBS"
+      run ninja -C builddir test
+    ;;
+  cmake-make)
+      run gen_cmake build
+      run make -C build -j"$JOBS"
+      run make -C build test
+    ;;
+  cmake-ninja)
+      run gen_cmake build -G Ninja
+      run ninja -C build -j"$JOBS"
+      run ninja -C build test
+    ;;
+esac
