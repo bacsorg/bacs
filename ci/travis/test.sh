6454e7b9b2f738681804865392011f423859441a
Aleksey Filippov <sarum9in@gmail.com>
2018-02-14 00:55:16 +0000

    Added libraries hints to CMake.

--- ci/travis/test.sh
+++ ci/travis/test.sh
@@ -6,7 +6,23 @@ function gen_cmake() (
   mkdir "$1"
   cd "$1"
   shift
-  cmake .. "$@"
+  cmake \
+      -DENABLE_WEB=OFF \
+      -DENABLE_CURL=OFF \
+      -DBoost_ADDITIONAL_VERSIONS="1.66.0" \
+      -DBOOST_ROOT="$BOOST_ROOT" \
+      -DBOOST_INCLUDEDIR="$BOOST_INCLUDEDIR" \
+      -DBOOST_LIBRARYDIR="$BOOST_LIBRARYDIR" \
+      -DBoost_NO_SYSTEM_PATHS=ON \
+      -DProtobuf_INCLUDE_DIR="$HOME_PREFIX/include" \
+      -DProtobuf_LIBRARIES="$HOME_PREFIX/lib/libprotobuf.so" \
+      -DGRPC_INCLUDE_DIR="$HOME_PREFIX/include" \
+      -DGPR_LIBRARY="$HOME_PREFIX/lib/libgpr.so" \
+      -DGRPC_LIBRARY="$HOME_PREFIX/lib/libgrpc.so" \
+      -DGRPCXX_LIBRARY="$HOME_PREFIX/lib/libgrpc++.so" \
+      -DGRPC_CXX_PLUGIN="$HOME_PREFIX/bin/grpc_cpp_plugin" \
+      -DGRPC_PYTHON_PLUGIN="$HOME_PREFIX/bin/grpc_python_plugin" \
+      .. "$@"
 )
 
 case "$BUILD" in
