42746d0026080297f46475dded0adbe25f52adb5
Aleksey Filippov <sarum9in@gmail.com>
2018-03-20 13:38:35 +0000

    Added bunsan::utility::makers::ninja.

---
+++ bunsan/utility/src/makers/ninja.cpp
@@ -0,0 +1,55 @@
+#include "ninja.hpp"
+
+#include <bunsan/config/cast.hpp>
+#include <bunsan/process/execute.hpp>
+#include <bunsan/static_initializer.hpp>
+
+#include <boost/filesystem/operations.hpp>
+
+using namespace bunsan::utility;
+
+BUNSAN_STATIC_INITIALIZER(bunsan_utility_makers_ninja, {
+  BUNSAN_FACTORY_REGISTER_TOKEN(maker, ninja, [](resolver &resolver_) {
+    return maker::make_shared<makers::ninja>(
+        resolver_.find_executable("ninja"));
+  })
+})
+
+makers::ninja::ninja(const boost::filesystem::path &exe) : m_exe(exe) {}
+
+std::vector<std::string> makers::ninja::arguments_(
+    const std::vector<std::string> &targets) const {
+  std::vector<std::string> arguments;
+  arguments.push_back(m_exe.filename().string());
+  if (m_config.jobs)
+    arguments.push_back("-j" +
+                        boost::lexical_cast<std::string>(m_config.jobs.get()));
+  arguments.insert(arguments.end(), m_config.targets.begin(),
+                   m_config.targets.end());
+  arguments.insert(arguments.end(), targets.begin(), targets.end());
+  return arguments;
+}
+
+void makers::ninja::exec(const boost::filesystem::path &cwd,
+                         const std::vector<std::string> &targets) {
+  try {
+    bunsan::process::context ctx;
+    ctx.executable(m_exe);
+    ctx.current_path(cwd);
+    ctx.arguments(arguments_(targets));
+    for (const auto &i : m_config.defines) {
+      // TODO arguments check
+      ctx.environment_set(i.first, i.second);
+    }
+    check_sync_execute_with_output(ctx);
+  } catch (std::exception &) {
+    BOOST_THROW_EXCEPTION(maker_exec_error()
+                          << maker_exec_error::cwd(cwd)
+                          << maker_exec_error::targets(targets)
+                          << enable_nested_current());
+  }
+}
+
+void makers::ninja::setup(const boost::property_tree::ptree &ptree) {
+  m_config = bunsan::config::load<config>(ptree);
+}
---
+++ bunsan/utility/src/makers/ninja.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <bunsan/utility/maker.hpp>
+
+#include <boost/optional.hpp>
+#include <boost/serialization/access.hpp>
+#include <boost/serialization/nvp.hpp>
+
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+namespace bunsan::utility::makers {
+
+class ninja : public maker {
+ public:
+  struct config {
+    template <typename Archive>
+    void serialize(Archive &ar, const unsigned int) {
+      ar & BOOST_SERIALIZATION_NVP(defines);
+      ar & BOOST_SERIALIZATION_NVP(targets);
+      ar & BOOST_SERIALIZATION_NVP(jobs);
+    }
+
+    /// for example {"DESTDIR": "/some/path"}
+    std::unordered_map<std::string, std::string> defines;
+    std::vector<std::string> targets;  ///< for example ["install"]
+    boost::optional<std::size_t> jobs;
+  };
+
+ public:
+  explicit ninja(const boost::filesystem::path &exe);
+
+  void exec(const boost::filesystem::path &cwd,
+            const std::vector<std::string> &targets) override;
+
+  void setup(const boost::property_tree::ptree &ptree) override;
+
+ private:
+  std::vector<std::string> arguments_(
+      const std::vector<std::string> &targets) const;
+
+ private:
+  const boost::filesystem::path m_exe;
+  config m_config;
+};
+
+}  // namespace bunsan::utility::makers
